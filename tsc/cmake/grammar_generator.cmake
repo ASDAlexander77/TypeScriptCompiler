0592535f915a245a44f38ec503775dd174e213d0
ODuzhar <oduzhar@sportingindex.com>
2021-01-17 02:12:18 +0000

    fixed compiling issues

---
+++ tsc/cmake/grammar_generator.cmake
@@ -0,0 +1,28 @@
+macro(grammar_generator)
+
+# generate lexer
+antlr4_generate( 
+   TypeScriptLexerANTLR
+   "${CMAKE_SOURCE_DIR}/tsc-grammar/TypeScriptLexerANTLR.g4"
+   LEXER
+   FALSE
+   FALSE
+   "typescript"
+   )
+ 
+# generate parser
+antlr4_generate( 
+   TypeScriptParserANTLR
+   "${CMAKE_SOURCE_DIR}/tsc-grammar/TypeScriptParserANTLR.g4"
+   PARSER
+   FALSE
+   TRUE
+   "typescript"
+   "${ANTLR4_TOKEN_FILES_TypeScriptLexerANTLR}"
+   "${ANTLR4_TOKEN_DIRECTORY_TypeScriptLexerANTLR}"
+   )
+
+list(FILTER ANTLR4_SRC_FILES_TypeScriptLexerANTLR INCLUDE REGEX ".*\.cpp$")
+list(FILTER ANTLR4_SRC_FILES_TypeScriptParserANTLR INCLUDE REGEX ".*\.cpp$")
+
+endmacro()
