c60445593b612acbf7eb273776942e3b455ca75e
ODuzhar <oduzhar@sportingindex.com>
2021-02-06 01:42:38 +0000

    added optional type

--- tsc/test/tester/tests/01arguments.ts
+++ tsc/test/tester/tests/01arguments.ts
@@ -1,19 +1,17 @@
-function defaultArgs1(x: number, y = 3, z = 7) {
+function defaultArgs1(x: number, y = 3, z = 7): number {
     return x;
 }
 
-function defaultArgs2(x: number, y = 3, z = 7) {
+function defaultArgs2(x: number, y = 3, z = 7): number {
     return y;
 }
 
-function defaultArgs3(x: number, y = 3, z = 7) {
+function defaultArgs3(x: number, y = 3, z = 7): number {
     return z;
 }
 
-function testDefaultArgs() {
+function main() {
     assert(defaultArgs1(1) == 1, "defl0")
     assert(defaultArgs2(1, 4) == 4, "defl1")
     assert(defaultArgs3(1, 4, 8) == 8, "defl2")
 }
-
-testDefaultArgs();
