6b03850956eeca9c8ddd977a7ee786bfa7695987
Alex D <oduzhar@sportingindex.com>
2021-03-11 23:30:40 +0000

    fixed test

--- tsc/test/tester/tests/01arguments.ts
+++ tsc/test/tester/tests/01arguments.ts
@@ -3,31 +3,29 @@ function defaultArgs(x: number, y = 3, z = 7) {
 }
 
 function main() {
-    assert(defaultArgs(1) == 11, "defl0");
-    assert(defaultArgs(1, 4) == 12, "defl1");
-    assert(defaultArgs(1, 4, 8) == 13, "defl2");
+    print("testDefaultArgs");
+    assert(defaultArgs(1) == 11, "defl0")
+    assert(defaultArgs(1, 4) == 12, "defl1")
+    assert(defaultArgs(1, 4, 8) == 13, "defl2")
 
-    /*
-    assert(optargs(1) == 1, "opt0");
-    assert(optargs(1, 2) == 3, "opt1");
-    assert(optargs(1, 2, 3) == 3, "opt2");
-    */
+    //assert(optargs(1) == 1, "opt0");
+    //assert(optargs(1, 2) == 3, "opt1");
+    //assert(optargs(1, 2, 3) == 3, "opt2");
 
-    /*
     assert(optstring(3) == 6, "os0")
     assert(optstring(3, "7") == 10, "os1")
-    */
-    assert(optstring2(3) == 6, "os0");
-    assert(optstring2(3, "7") == 10, "os1");
+    assert(optstring2(3) == 6, "os0")
+    assert(optstring2(3, "7") == 10, "os1")
 }
 
+/*
 function optargs(x: number, y?: number, z?: number) {
     if (y == undefined)
-        y = 0;
+        y = 0
     return x + y;
 }
+*/
 
-// TODO: when string is undef it should be null ptr as well
 function optstring(x: number, s?: string) {
     if (s != null) {
         return parseInt(s) + x;
