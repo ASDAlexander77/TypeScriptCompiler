40aa4ac341c04810a5cd93e4dfc19b18f91dffad
Alex D <oduzhar@sportingindex.com>
2021-03-13 23:27:32 +0000

    added test extra

--- tsc/test/tester/tests/00globals.ts
+++ tsc/test/tester/tests/00globals.ts
@@ -4,4 +4,6 @@ const glb2 = 2;
 function main() {    
     assert(glb1 == 1, "Failed. glb1");
     assert(glb2 == 2, "Failed. glb2");
+    glb1 = glb1 + 1;
+    assert(glb1 == 2, "Failed. glb1++");
 }
