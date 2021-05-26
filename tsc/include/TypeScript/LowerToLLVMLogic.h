48b6f14ea049cefc18ecbf842ea0aa6bd678f592
Alex D <oduzhar@sportingindex.com>
2021-05-26 16:52:45 +0100

    implemneted index access for array type

--- tsc/include/TypeScript/LowerToLLVMLogic.h
+++ tsc/include/TypeScript/LowerToLLVMLogic.h
@@ -197,6 +197,136 @@ namespace typescript
         }
     };    
 
+    class CodeLogicHelper
+    {
+        Operation *op;
+        PatternRewriter &rewriter;
+    public:        
+        CodeLogicHelper(Operation *op, PatternRewriter &rewriter) : op(op), rewriter(rewriter) {}
+
+        Value createIConstantOf(unsigned width, unsigned value)
+        {
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(width), rewriter.getIntegerAttr(rewriter.getIntegerType(width), value));
+        }
+
+        Value createFConstantOf(unsigned width, double value)
+        {
+            auto ftype = rewriter.getF32Type();
+            if (width == 16)
+            {
+                ftype = rewriter.getF16Type();
+            }
+            else if (width == 64)
+            {
+                ftype = rewriter.getF64Type();
+            }
+            else if (width == 128)
+            {
+                ftype = rewriter.getF128Type();
+            }
+
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), ftype, rewriter.getFloatAttr(ftype, value));
+        }
+
+        Value createI8ConstantOf(unsigned value)
+        {
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(8), rewriter.getIntegerAttr(rewriter.getIntegerType(8), value));
+        }
+
+        Value createI32ConstantOf(unsigned value)
+        {
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(32), rewriter.getIntegerAttr(rewriter.getI32Type(), value));
+        }
+
+        Value createI64ConstantOf(unsigned value)
+        {
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(64), rewriter.getIntegerAttr(rewriter.getI64Type(), value));
+        }
+
+        Value createI1ConstantOf(bool value)
+        {
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(1), rewriter.getIntegerAttr(rewriter.getI1Type(), value));
+        }    
+
+        Value createF32ConstantOf(float value)
+        {
+            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getF32Type(), rewriter.getIntegerAttr(rewriter.getF32Type(), value));
+        }
+
+        Value castToI8Ptr(mlir::Value value)
+        {
+            TypeHelper th(rewriter);
+            return rewriter.create<LLVM::BitcastOp>(op->getLoc(), th.getI8PtrType(), value);
+        }
+
+        Value conditionalExpressionLowering(
+            Type type, Value condition,
+            function_ref<Value(OpBuilder &, Location)> thenBuilder,
+            function_ref<Value(OpBuilder &, Location)> elseBuilder)
+        {
+            auto loc = op->getLoc();
+
+            // Split block
+            auto *opBlock = rewriter.getInsertionBlock();
+            auto opPosition = rewriter.getInsertionPoint();
+            auto *continuationBlock = rewriter.splitBlock(opBlock, opPosition);
+
+            // then block
+            auto *thenBlock = rewriter.createBlock(continuationBlock);
+            auto thenValue = thenBuilder(rewriter, loc);
+
+            // else block
+            auto *elseBlock = rewriter.createBlock(continuationBlock);
+            auto elseValue = elseBuilder(rewriter, loc);
+
+            // result block
+            auto *resultBlock = rewriter.createBlock(continuationBlock, TypeRange{type});
+            rewriter.create<LLVM::BrOp>(
+                loc,
+                ValueRange{},
+                continuationBlock);
+
+            rewriter.setInsertionPointToEnd(thenBlock);
+            rewriter.create<LLVM::BrOp>(
+                loc,
+                ValueRange{thenValue},
+                resultBlock);
+
+            rewriter.setInsertionPointToEnd(elseBlock);
+            rewriter.create<LLVM::BrOp>(
+                loc,
+                ValueRange{elseValue},
+                resultBlock);
+
+            // Generate assertion test.
+            rewriter.setInsertionPointToEnd(opBlock);
+            rewriter.create<LLVM::CondBrOp>(
+                loc,
+                condition,
+                thenBlock,
+                elseBlock);
+
+            rewriter.setInsertionPointToStart(continuationBlock);
+
+            return resultBlock->getArguments().front();
+        }
+
+        template <typename OpTy>
+        void saveResult(OpTy& op, Value result)
+        {
+            auto defOp = op.operand1().getDefiningOp();
+            // TODO: finish it for field access
+            if (auto loadOp = dyn_cast<mlir_ts::LoadOp>(defOp))
+            {
+                rewriter.create<mlir_ts::StoreOp>(op->getLoc(), result, loadOp.reference());
+            }
+            else
+            {
+                llvm_unreachable("not implemented");
+            }
+        }
+    };
+
     class LLVMCodeHelper
     {
         Operation *op;
@@ -479,18 +609,29 @@ namespace typescript
         {
             TypeHelper th(rewriter);
             TypeConverterHelper tch(typeConverter);
+            CodeLogicHelper clh(op, rewriter);
 
             auto loc = op->getLoc();
-            auto globalPtr = arrayOrStringOrTuple;
 
             assert(elementRefType.isa<mlir_ts::RefType>());
 
             auto ptrType = tch.convertType(elementRefType);
 
+            auto dataPtr = arrayOrStringOrTuple;
+            if (auto arrayType = arrayOrStringOrTuple.getType().isa<mlir_ts::ArrayType>())
+            {
+                // extract pointer from struct
+                dataPtr = rewriter.create<LLVM::ExtractValueOp>(
+                    loc, 
+                    ptrType, 
+                    arrayOrStringOrTuple, 
+                    rewriter.getI32ArrayAttr(mlir::ArrayRef<int32_t>(0)));
+            }
+
             auto addr = rewriter.create<LLVM::GEPOp>(
                 loc,
                 ptrType, 
-                globalPtr,
+                dataPtr,
                 ValueRange{index});
 
             return addr;            
@@ -524,136 +665,6 @@ namespace typescript
 
             return addr;            
         }        
-    };
-
-    class CodeLogicHelper
-    {
-        Operation *op;
-        PatternRewriter &rewriter;
-    public:        
-        CodeLogicHelper(Operation *op, PatternRewriter &rewriter) : op(op), rewriter(rewriter) {}
-
-        Value createIConstantOf(unsigned width, unsigned value)
-        {
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(width), rewriter.getIntegerAttr(rewriter.getIntegerType(width), value));
-        }
-
-        Value createFConstantOf(unsigned width, double value)
-        {
-            auto ftype = rewriter.getF32Type();
-            if (width == 16)
-            {
-                ftype = rewriter.getF16Type();
-            }
-            else if (width == 64)
-            {
-                ftype = rewriter.getF64Type();
-            }
-            else if (width == 128)
-            {
-                ftype = rewriter.getF128Type();
-            }
-
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), ftype, rewriter.getFloatAttr(ftype, value));
-        }
-
-        Value createI8ConstantOf(unsigned value)
-        {
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(8), rewriter.getIntegerAttr(rewriter.getIntegerType(8), value));
-        }
-
-        Value createI32ConstantOf(unsigned value)
-        {
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(32), rewriter.getIntegerAttr(rewriter.getI32Type(), value));
-        }
-
-        Value createI64ConstantOf(unsigned value)
-        {
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(64), rewriter.getIntegerAttr(rewriter.getI64Type(), value));
-        }
-
-        Value createI1ConstantOf(bool value)
-        {
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getIntegerType(1), rewriter.getIntegerAttr(rewriter.getI1Type(), value));
-        }    
-
-        Value createF32ConstantOf(float value)
-        {
-            return rewriter.create<LLVM::ConstantOp>(op->getLoc(), rewriter.getF32Type(), rewriter.getIntegerAttr(rewriter.getF32Type(), value));
-        }
-
-        Value castToI8Ptr(mlir::Value value)
-        {
-            TypeHelper th(rewriter);
-            return rewriter.create<LLVM::BitcastOp>(op->getLoc(), th.getI8PtrType(), value);
-        }
-
-        Value conditionalExpressionLowering(
-            Type type, Value condition,
-            function_ref<Value(OpBuilder &, Location)> thenBuilder,
-            function_ref<Value(OpBuilder &, Location)> elseBuilder)
-        {
-            auto loc = op->getLoc();
-
-            // Split block
-            auto *opBlock = rewriter.getInsertionBlock();
-            auto opPosition = rewriter.getInsertionPoint();
-            auto *continuationBlock = rewriter.splitBlock(opBlock, opPosition);
-
-            // then block
-            auto *thenBlock = rewriter.createBlock(continuationBlock);
-            auto thenValue = thenBuilder(rewriter, loc);
-
-            // else block
-            auto *elseBlock = rewriter.createBlock(continuationBlock);
-            auto elseValue = elseBuilder(rewriter, loc);
-
-            // result block
-            auto *resultBlock = rewriter.createBlock(continuationBlock, TypeRange{type});
-            rewriter.create<LLVM::BrOp>(
-                loc,
-                ValueRange{},
-                continuationBlock);
-
-            rewriter.setInsertionPointToEnd(thenBlock);
-            rewriter.create<LLVM::BrOp>(
-                loc,
-                ValueRange{thenValue},
-                resultBlock);
-
-            rewriter.setInsertionPointToEnd(elseBlock);
-            rewriter.create<LLVM::BrOp>(
-                loc,
-                ValueRange{elseValue},
-                resultBlock);
-
-            // Generate assertion test.
-            rewriter.setInsertionPointToEnd(opBlock);
-            rewriter.create<LLVM::CondBrOp>(
-                loc,
-                condition,
-                thenBlock,
-                elseBlock);
-
-            rewriter.setInsertionPointToStart(continuationBlock);
-
-            return resultBlock->getArguments().front();
-        }
-
-        template <typename OpTy>
-        void saveResult(OpTy& op, Value result)
-        {
-            auto defOp = op.operand1().getDefiningOp();
-            // TODO: finish it for field access
-            if (auto loadOp = dyn_cast<mlir_ts::LoadOp>(defOp))
-            {
-                rewriter.create<mlir_ts::StoreOp>(op->getLoc(), result, loadOp.reference());
-            }
-            else
-            {
-                llvm_unreachable("not implemented");
-            }
-        }
     };
 
     class CastLogicHelper
