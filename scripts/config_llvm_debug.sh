9a62078579f28de5f2b6cc024dd8a44cce4e7ebb
Alex D <oduzhar@sportingindex.com>
2021-06-25 00:29:13 +0100

    fixes

--- scripts/config_llvm_debug.sh
+++ scripts/config_llvm_debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p __build/llvm-ninja/debug
 cd __build/llvm-ninja
-cmake ../../3rdParty/llvm-project/llvm -G "Ninja" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../../3rdParty/llvm-ninja/debug -DLLVM_INSTALL_UTILS=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="lld;mlir"
+cmake ../../3rdParty/llvm-project/llvm -G "Ninja" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_C_COMPILER=clang-12 -DCMAKE_CXX_COMPILER=clang++-12 -DLLVM_ENABLE_LLD=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../../3rdParty/llvm-ninja/debug -DLLVM_INSTALL_UTILS=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="lld;mlir"
 
