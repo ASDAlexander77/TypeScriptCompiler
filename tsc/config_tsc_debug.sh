44f06209fc1a63ed1265da7068125bb21a379866
Alex D <oduzhar@sportingindex.com>
2021-06-25 16:41:16 +0100

    final try

--- tsc/config_tsc_debug.sh
+++ tsc/config_tsc_debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p ../__build/tsc-ninja
 cd ../__build/tsc-ninja
-cmake ../../tsc -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_C_COMPILER=clang-12 -DCMAKE_CXX_COMPILER=clang++-12 -DLLVM_ENABLE_LLD=ON
+cmake ../../tsc -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev
 
