2dc4ef48b8e2e0741a62ebe4c65d5af1ae3a0769
Alex D <oduzhar@sportingindex.com>
2021-06-25 11:01:48 +0100

    more fixes

--- tsc/tsc-new-parser/parser_types.h
+++ tsc/tsc-new-parser/parser_types.h
@@ -44,7 +44,7 @@ struct ReadonlyTextRange : TextRange
 
 template <typename T /*extends Node*/> struct ReadonlyArray : std::vector<T>
 {
-    using vector<T>::vector;
+    using std::vector<T>::vector;
 };
 
 template <typename T /*extends Node*/> struct NodeArray : ReadonlyArray<T>, TextRange
