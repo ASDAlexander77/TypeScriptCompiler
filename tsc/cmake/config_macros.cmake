1216480563cb63bb1960d3a32e593f495bca8048
Alex D <oduzhar@sportingindex.com>
2021-06-25 11:38:48 +0100

    more fixes

--- tsc/cmake/config_macros.cmake
+++ tsc/cmake/config_macros.cmake
@@ -3,7 +3,7 @@ macro(set_Options)
 if(MSVC)
     SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 else()
-    SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-switch -Wno-unused-function")
+    SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-switch -Wno-unused-function -lstdc++fs")
 endif()
 
 endmacro()
