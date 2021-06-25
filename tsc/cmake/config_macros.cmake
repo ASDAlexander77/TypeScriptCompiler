f546f56d7ea8fdd13e07f1be0a4fc39048e7ecbe
Alex D <oduzhar@sportingindex.com>
2021-06-25 19:31:39 +0100

    more fixes (gcc issues with symbols)

--- tsc/cmake/config_macros.cmake
+++ tsc/cmake/config_macros.cmake
@@ -5,17 +5,7 @@ if(MSVC)
 else()
     #SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-switch -Wno-unused-function -Wno-unused-result -Wno-unused-variable -Wno-unused-private-field -Wno-sign-compare -Wno-implicit-fallthrough -Wno-logical-op-parentheses -Wno-parentheses -Wno-unused-command-line-argument -lstdc++fs")
     SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-switch -Wno-unused-function -Wno-unused-result -Wno-unused-variable -Wno-sign-compare -Wno-implicit-fallthrough -Wno-parentheses -Wno-type-limits -Wno-unused-but-set-variable -lstdc++fs")
-endif()
-
-endmacro()
-
-macro(set_Libs list)
-
-if(MSVC)
-else()
-    #set(list "${list} stdc++fs")
     link_libraries(stdc++fs)
 endif()
 
 endmacro()
-
