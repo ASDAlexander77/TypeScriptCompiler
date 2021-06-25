cee99754ecdf16c5f53392759d94acc1d17f0af8
Alex D <oduzhar@sportingindex.com>
2021-06-25 15:10:26 +0100

    more fixes

--- tsc/cmake/config_macros.cmake
+++ tsc/cmake/config_macros.cmake
@@ -3,9 +3,17 @@ macro(set_Options)
 if(MSVC)
     SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 else()
-    SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-switch -Wno-unused-function -Wno-unused-result -Wno-sign-compare -Wno-implicit-fallthrough -Wno-logical-op-parentheses -Wno-parentheses -lstdc++fs")
+    SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-switch -Wno-unused-function -Wno-unused-result -Wno-sign-compare -Wno-implicit-fallthrough -Wno-logical-op-parentheses -Wno-parentheses")
 endif()
 
 endmacro()
 
+macro(set_Libs list)
+
+if(MSVC)
+else()
+    set(list "${list} stdc++fs")
+endif()
+
+endmacro()
 
