12098ba56bea1106f12a2c01d0179c427ccfe91f
Alex D <oduzhar@sportingindex.com>
2021-06-26 19:16:42 +0100

    fix to build in unix

--- tsc/build_tsc_debug.sh
+++ tsc/build_tsc_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ../__build/tsc-ninja
 cmake --build . --config Debug -j 8
-sh -f ../scripts/separate_debug_info.sh ../__build/tsc-ninja/bin/tsc
-sh -f ../scripts/separate_debug_info.sh ../__build/tsc-ninja/bin/tsc-opt
-sh -f ../scripts/separate_debug_info.sh ../__build/tsc-ninja/bin/tsc-translate
+bash -f ../scripts/separate_debug_info.sh ../__build/tsc-ninja/bin/tsc
+bash -f ../scripts/separate_debug_info.sh ../__build/tsc-ninja/bin/tsc-opt
+bash -f ../scripts/separate_debug_info.sh ../__build/tsc-ninja/bin/tsc-translate
