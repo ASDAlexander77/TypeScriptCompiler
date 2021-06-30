1a0c4bfa9e68d5e7d6d1d98eada661dc65f11c49
Alex D <oduzhar@sportingindex.com>
2021-06-30 17:20:14 +0100

    added test for namespace

--- tsc/test/tester/tests/00ns.ts
+++ tsc/test/tester/tests/00ns.ts
@@ -12,8 +12,15 @@ function f() {
     print("hello");
 }
 
+namespace nn1 {
+    type Type1 = 1;
+}
+
+function f1(p: nn1.Type1) {}
+
 function main() {
     f();
     nn.fff();
+    f1(10);
     print("done.");
 }
