8581fdf3694b69ffddec1c76825808608305c01c
Alex D <oduzhar@sportingindex.com>
2021-07-21 18:32:15 +0100

    fixed test_helper sleep cmd

--- tsc/test/tester/test-runner.cpp
+++ tsc/test/tester/test-runner.cpp
@@ -10,7 +10,13 @@
 #include <stdexcept>
 #include <string>
 
+#ifdef WIN32
 #include <windows.h>
+#elif _POSIX_C_SOURCE >= 199309L
+#include <time.h> // for nanosleep
+#else
+#include <unistd.h> // for usleep
+#endif
 
 #if __cplusplus >= 201703L
 #include <filesystem>
@@ -69,6 +75,22 @@ bool hasEnding(std::string const &fullString, std::string const &ending)
     }
 }
 
+void sleep_ms(int milliseconds)
+{ // cross-platform sleep function
+#ifdef WIN32
+    Sleep(milliseconds);
+#elif _POSIX_C_SOURCE >= 199309L
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+#else
+    if (milliseconds >= 1000)
+        sleep(milliseconds / 1000);
+    usleep((milliseconds % 1000) * 1000);
+#endif
+}
+
 std::string exec(std::string cmd)
 {
     auto retry = 3;
@@ -101,7 +123,7 @@ std::string exec(std::string cmd)
                 else
                 {
                     std::cerr << "retrying..." << std::endl;
-                    Sleep(1000);
+                    sleep_ms(1000);
                     continue;
                 }
             }
