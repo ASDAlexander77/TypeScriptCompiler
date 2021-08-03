790f6d073e172d5a4adc72651bf9278e27557657
Alex D <oduzhar@sportingindex.com>
2021-08-03 09:29:45 +0100

    added script

---
+++ tsc/build_tsc_release.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ../__build/tsc-ninja
+cmake --build . --config Release -j 8
---
+++ tsc/config_tsc_release.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p ../__build/tsc-ninja
+cd ../__build/tsc-ninja
+cmake ../../tsc -G "Ninja" -DCMAKE_BUILD_TYPE=Release -Wno-dev
+
