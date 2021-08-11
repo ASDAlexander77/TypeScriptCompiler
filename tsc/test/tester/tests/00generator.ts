36aaa3f6dff00dcad1e362e56baf5baabd72f166
Alex D <oduzhar@sportingindex.com>
2021-08-11 17:31:22 +0100

    added test

--- tsc/test/tester/tests/00generator.ts
+++ tsc/test/tester/tests/00generator.ts
@@ -1,6 +1,6 @@
 type int = 1;
 
-function makeRangeIterator(start: int, end: int, step: int) {
+function makeRangeIterator1(start: int, end: int, step: int) {
     const rangeIterator = {
         nextIndex: start,
         end: end,
@@ -21,9 +21,9 @@ function makeRangeIterator(start: int, end: int, step: int) {
     return rangeIterator;
 }
 
-function main() {
+function main1() {
     // DO NOT PUT CONST, otherwise you can't edit
-    let it = makeRangeIterator(1, 10, 2);
+    let it = makeRangeIterator1(1, 10, 2);
 
     let result = it.next();
     while (!result.done) {
@@ -32,4 +32,43 @@ function main() {
     }
 
     print("done.");
+}
+
+function makeRangeIterator2(start: int, end: int, step: int) {
+    const rangeIterator = {
+        nextIndex: start,
+        end: end,
+        step: step,
+        iterationCount: 0,
+        next: function () {
+            let result: [value: int, done: boolean];
+            if (this.nextIndex < this.end) {
+                result = [this.nextIndex, false];
+                this.nextIndex += this.step;
+                this.iterationCount++;
+            } else {
+                result = [this.iterationCount, true];
+            }
+
+            return result;
+        },
+    };
+
+    return rangeIterator;
+}
+
+function main2() {
+    // DO NOT PUT CONST, otherwise you can't edit
+    let it = makeRangeIterator2(1, 10, 2);
+
+    let result = it.next();
+    while (!result.done) {
+        print(result.value); // 1 3 5 7 9
+        result = it.next();
+    }
+}
+
+function main() {
+    main1();
+    main2();
 }
