7b03dd7d75ecddebaefa283b9b474e1e32575cae
Alex D <oduzhar@sportingindex.com>
2021-08-16 02:22:48 +0100

    added test

--- tsc/test/tester/tests/00funcs_capture.ts
+++ tsc/test/tester/tests/00funcs_capture.ts
@@ -1,11 +1,31 @@
-function main() {
+function main_func() {
     let a = 10;
 
     function f() {
+        assert(a == 10);
         print(a);
     }
 
     f();
+}
 
+function main_func_in_object() {
+    let a = 10;
+
+    const s = {
+        in: 20,
+        f() {
+            assert(this.in == 20);
+            assert(a == 10);
+            print(this.in, a);
+        },
+    };
+
+    s.f();
+}
+
+function main() {
+    main_func();
+    main_func_in_object();
     print("done.");
 }
