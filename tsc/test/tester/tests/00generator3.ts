246a46d99376ad116f21fb6dafdc9055d4b69aff
Alex D <oduzhar@sportingindex.com>
2021-08-24 13:43:54 +0100

    added test

--- tsc/test/tester/tests/00generator3.ts
+++ tsc/test/tester/tests/00generator3.ts
@@ -10,7 +10,7 @@ class IntIter {
     }
 }
 
-function main() {
+function main1() {
     let it = new IntIter();
 
     let count = 0;
@@ -28,6 +28,86 @@ function main() {
     }
 
     assert(count == 5);
+}
 
+// ver 2
+function foo2()
+{
+	return {
+		step: 0,
+		next() {
+			switch (this.step++)
+			{
+			  	//yield 1;
+				case 0: 
+					return { value: 1, done: false; };
+			  	//yield 2;
+				case 1: 
+					return { value: 2, done: false; };
+			  	//yield 3;
+				case 2:
+					return { value: 3, done: false; };
+				default:
+					return { value: 3, done: true; };					
+			}
+		}
+	}
+
+};
+
+function main2() {
+
+    let count = 0;
+    for (const o of foo2())
+    {
+    	print (o);
+        count++;
+    }
+
+    assert(count == 3);
+}
+
+// ver 3
+function foo3()
+{
+    let step = 0;
+	return {
+		next() {
+			switch (step++)
+			{
+			  	//yield 1;
+				case 0: 
+					return { value: 1, done: false; };
+			  	//yield 2;
+				case 1: 
+					return { value: 2, done: false; };
+			  	//yield 3;
+				case 2:
+					return { value: 3, done: false; };
+				default:
+					return { value: 3, done: true; };					
+			}
+		}
+	}
+
+};
+
+function main3() {
+
+    let count = 0;
+    for (const o of foo3())
+    {
+    	print (o);
+        count++;
+    }
+
+    assert(count == 3);    
+}
+
+function main()
+{
+    main1();
+    main2();
+    main3();
     print("done.");
 }
