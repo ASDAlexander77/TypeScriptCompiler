78e7584faa47a85af8240e44677d6fab78886ac6
Alex D <oduzhar@sportingindex.com>
2021-08-31 16:46:06 +0100

    more fixes

--- scripts/build_gc_release.sh
+++ scripts/build_gc_release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p __build/gc-ninja-release
+mkdir -p 3rdParty/gc/release
 cd __build/gc-ninja-release
 cmake ../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Release -Wno-dev -DCMAKE_INSTALL_PREFIX=../../3rdParty/gc/release
 cmake --build . --config Release -j 8
