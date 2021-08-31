473f1e015dd786665069a529412598bacd690ecf
Alex D <oduzhar@sportingindex.com>
2021-08-31 16:48:06 +0100

    more fixes

--- scripts/build_gc_debug.sh
+++ scripts/build_gc_debug.sh
@@ -4,4 +4,4 @@ mkdir -p 3rdParty/gc/debug
 cd __build/gc-ninja-debug
 cmake ../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_INSTALL_PREFIX=../../3rdParty/gc/debug
 cmake --build . --config Debug -j 8
-cp lib* ../../3rdParty/gc/debug/
+cp ./lib* ../../3rdParty/gc/debug/
