9f60079bf129a85ba548b5f81cec2595dd98ec54
Alex D <oduzhar@sportingindex.com>
2021-08-31 16:37:51 +0100

    fixes to build script

--- scripts/build_gc_debug.sh
+++ scripts/build_gc_debug.sh
@@ -3,4 +3,4 @@ mkdir -p __build/gc-ninja-debug
 cd __build/gc-ninja-debug
 cmake ../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_INSTALL_PREFIX=../../3rdParty/gc/debug
 cmake --build . --config Debug -j 8
-
+copy lib* ../../3rdParty/gc/debug/
--- scripts/build_gc_release.sh
+++ scripts/build_gc_release.sh
@@ -3,4 +3,4 @@ mkdir -p __build/gc-ninja-release
 cd __build/gc-ninja-release
 cmake ../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Release -Wno-dev -DCMAKE_INSTALL_PREFIX=../../3rdParty/gc/release
 cmake --build . --config Release -j 8
-
+copy lib* ../../3rdParty/gc/release/
