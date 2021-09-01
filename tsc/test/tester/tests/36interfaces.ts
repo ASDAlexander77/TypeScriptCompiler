49851428eb375a53f0584e74b8e3bcdb36293250
Alex D <oduzhar@sportingindex.com>
2021-09-01 19:27:44 +0100

    test fixed

--- tsc/test/tester/tests/36interfaces.ts
+++ tsc/test/tester/tests/36interfaces.ts
@@ -34,13 +34,13 @@ namespace Ifaces {
     export function run() {
         print("Ifaces.run");
         let a = new A();
-        assert("" + foo(a) + "X" == "12.QA42.X");
+        assert("" + foo(a) + "X" == "12QA42X");
         assert((a as IFoo).twoArg(1, 2) == 1, "t");
         a = new B();
-        assert("" + foo(a) + "X" == "13.QA42.X", "b");
+        assert("" + foo(a) + "X" == "13QA42X", "b");
         let q = a as IFoo;
         q.baz = "Z";
-        assert("" + foo(q) + "X" == "13.Z42.X", "x");
+        assert("" + foo(q) + "X" == "13Z42X", "x");
         print("Ifaces.runDONE");
     }
 }
