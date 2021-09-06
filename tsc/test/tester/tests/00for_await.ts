b87c50a95c25445409436bebda9b96b019314983
Alex D <oduzhar@sportingindex.com>
2021-09-06 09:29:43 +0100

    changing test

--- tsc/test/tester/tests/00for_await.ts
+++ tsc/test/tester/tests/00for_await.ts
@@ -1,4 +1,11 @@
-function main() {
+function main1() {
+    for await (const v of [1, 2, 3, 4, 5]) {
+        // TODO: if I add count++, it will work
+        print(v);
+    }
+}
+
+function main2() {
     let count = 0;
     for await (const v of [1, 2, 3, 4, 5]) {
         print(v);
@@ -7,6 +14,11 @@ function main() {
     }
 
     assert(count == 5);
+}
+
+function main() {
+    main1();
+    main2();
 
     print("done.");
 }
