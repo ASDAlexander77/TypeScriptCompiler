4d098bc79385688a4c30afdfe37dba43a4796a98
Alex D <oduzhar@sportingindex.com>
2021-09-06 14:37:56 +0100

    adding scripts to build gc with threads

--- prepare_3rdParty_debug.sh
+++ prepare_3rdParty_debug.sh
@@ -7,7 +7,10 @@ echo "Building LLVM (Debug)"
 ./scripts/build_llvm_debug.sh
 echo "Building GC (Debug)"
 curl -o gc-8.0.4.tar.gz https://www.hboehm.info/gc/gc_source/gc-8.0.4.tar.gz
+curl -o libatomic_ops-7.6.10.tar.gz https://www.hboehm.info/gc/gc_source/libatomic_ops-7.6.10.tar.gz
 tar -xvzf gc-8.0.4.tar.gz -C ./3rdParty/
+tar -xvzf libatomic_ops-7.6.10.tar.gz -C ./3rdParty/
+cp -a ./3rdParty/libatomic_ops-7.6.10/ ./3rdParty/gc-8.0.4/libatomic_ops/
 ./scripts/build_gc_debug.sh
 
 
--- scripts/build_gc_debug.sh
+++ scripts/build_gc_debug.sh
@@ -2,6 +2,6 @@
 mkdir -p __build/gc-ninja-debug
 mkdir -p 3rdParty/gc/debug
 cd __build/gc-ninja-debug
-cmake ../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=../../3rdParty/gc/debug
+cmake ../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=../../3rdParty/gc/debug -Denable_threads=ON
 cmake --build . --config Debug -j 8
 cp ./lib* ../../3rdParty/gc/debug/
