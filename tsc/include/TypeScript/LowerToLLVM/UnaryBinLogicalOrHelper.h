232a7a59a01a9dd70caa44fd706c4eaa7d644943
AlexD <duzhar@googlemail.com>
2021-09-11 21:42:07 +0100

    fixes

--- tsc/include/TypeScript/LowerToLLVM/UnaryBinLogicalOrHelper.h
+++ tsc/include/TypeScript/LowerToLLVM/UnaryBinLogicalOrHelper.h
@@ -7,18 +7,6 @@
 #include "TypeScript/TypeScriptDialect.h"
 #include "TypeScript/TypeScriptOps.h"
 
-#include "TypeScript/LowerToLLVM/TypeHelper.h"
-#include "TypeScript/LowerToLLVM/TypeConverterHelper.h"
-#include "TypeScript/LowerToLLVM/LLVMTypeConverterHelper.h"
-#include "TypeScript/LowerToLLVM/CodeLogicHelper.h"
-#include "TypeScript/LowerToLLVM/LLVMCodeHelper.h"
-#include "TypeScript/LowerToLLVM/LLVMRTTIHelperVCWin32.h"
-#include "TypeScript/LowerToLLVM/AssertLogic.h"
-#include "TypeScript/LowerToLLVM/ConvertLogic.h"
-#include "TypeScript/LowerToLLVM/CastLogicHelper.h"
-#include "TypeScript/LowerToLLVM/OptionalLogicHelper.h"
-#include "TypeScript/LowerToLLVM/TypeOfOpHelper.h"
-
 using namespace mlir;
 namespace mlir_ts = mlir::typescript;
 
