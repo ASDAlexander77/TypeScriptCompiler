48469470a0ee859de988d7e3c53875b125cdf602
AlexD <duzhar@googlemail.com>
2021-09-12 16:19:29 +0100

    fixes

--- prepare_3rdParty_release.sh
+++ prepare_3rdParty_release.sh
@@ -11,7 +11,7 @@ curl -o libatomic_ops-7.6.10.tar.gz https://www.hboehm.info/gc/gc_source/libatom
 tar -xvzf gc-8.0.4.tar.gz -C ./3rdParty/
 tar -xvzf libatomic_ops-7.6.10.tar.gz -C ./3rdParty/
 cp -a ./3rdParty/libatomic_ops-7.6.10/ ./3rdParty/gc-8.0.4/libatomic_ops/
-cp -a ./docs/fix/gc/ ./3rdParty/gc-8.0.4/
+cp -ar ./docs/fix/gc/* ./3rdParty/gc-8.0.4/
 ./scripts/build_gc_release.sh
 
 
