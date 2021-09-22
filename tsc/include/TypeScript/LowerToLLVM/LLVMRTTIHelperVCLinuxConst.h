032cc9b266d176871f775124fcdea58e1cf7ef7f
Alex D <duzhar@googlemail.com>
2021-09-22 15:08:55 +0100

    added info type

--- tsc/include/TypeScript/LowerToLLVM/LLVMRTTIHelperVCLinuxConst.h
+++ tsc/include/TypeScript/LowerToLLVM/LLVMRTTIHelperVCLinuxConst.h
@@ -5,22 +5,22 @@ namespace typescript
 {
 namespace F32Type
 {
-constexpr auto typeName = "_ZTIf";
+constexpr auto typeName = "_ZTId";
 } // namespace F32Type
 
 namespace I32Type
 {
-constexpr auto typeName = "_ZTId";
+constexpr auto typeName = "_ZTIi";
 } // namespace I32Type
 
 namespace StringType
 {
-constexpr auto typeName = "";
+constexpr auto typeName = "_ZTIPKc";
 } // namespace StringType
 
 namespace I8PtrType
 {
-constexpr auto typeName = "";
+constexpr auto typeName = "_ZTIPv";
 } // namespace I8PtrType
 
 namespace ClassType
