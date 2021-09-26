dfd3eb4a54a9f7d340e010e59ebc7c74f96fef80
AlexD <duzhar@googlemail.com>
2021-09-26 20:21:07 +0100

    added experiment build for wasm in windows (not working)

---
+++ scripts/config_llvm_wasm_debug.bat
@@ -0,0 +1,6 @@
+pushd
+mkdir __build\llvm-wasm\debug
+cd __build\llvm-wasm
+emcmake cmake ..\..\3rdParty\llvm-project\llvm -G "Visual Studio 16 2019" -A x64 -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="WebAssembly" -DCMAKE_BUILD_TYPE=Debug -Thost=x64 -DCMAKE_INSTALL_PREFIX=../../3rdParty/llvm-wasm/debug -DLLVM_INSTALL_UTILS=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PLUGINS=ON -DLLVM_ENABLE_PROJECTS="mlir" -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_REQUIRES_RTTI=ON -DLLVM_ENABLE_PIC=ON
+popd
+
