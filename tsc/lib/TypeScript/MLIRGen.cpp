b516e1aedad7083e0f90d322e88e4b5ab22ced14
AlexD <duzhar@googlemail.com>
2021-11-05 18:07:06 +0000

    progress

--- tsc/lib/TypeScript/MLIRGen.cpp
+++ tsc/lib/TypeScript/MLIRGen.cpp
@@ -272,6 +272,17 @@ class MLIRGenImpl
             return mlir::failure();
         }
 
+        // relocate ConstantOps, VariableOps
+
+        for (auto &op : theModule)
+        {
+            if (auto funcOp = dyn_cast_or_null<mlir_ts::FuncOp>(&op))
+            {
+                // relocate
+                relocateConsts(funcOp);
+            }
+        }
+
         return mlir::success();
     }
 
