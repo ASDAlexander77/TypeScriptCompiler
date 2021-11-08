75c6ab4006edbc31f6eca5277d3fe2d3da1d6a18
AlexD <duzhar@googlemail.com>
2021-11-08 19:37:32 +0000

    updated test case

--- tsc/test/tester/tests/00class_new.ts
+++ tsc/test/tester/tests/00class_new.ts
@@ -15,10 +15,25 @@ class Rectangle {
         return this.height * this.width;
     }
 }
+class Vector {
+    constructor(public x: number,
+        public y: number,
+        public z: number) {
+    }
+
+    static def = new Vector(0, 0, 0);
+
+    times() { return Vector.def; }
+}
 
 function main() {
     const square = new Rectangle(10, 10);
     print(square.area); // 100
     assert(square.area == 100.0);
+
+    // if it is compiled it is fine
+    const s = new Vector(1, 2, 3);
+    print(s.x, s.y, s.z);
+
     print("done.");
 }
