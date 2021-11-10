04c47bf78b648971b57e554afcfc830f8cbff5a4
AlexD <duzhar@googlemail.com>
2021-11-10 12:17:34 +0000

    added extra test

--- tsc/test/tester/tests/00object_global.ts
+++ tsc/test/tester/tests/00object_global.ts
@@ -2,8 +2,25 @@ let a = {
     a: 1
 };
 
+let deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+    createCardPicker: function () {
+        // NOTE: the line below is now an arrow function, allowing us to capture 'this' right here
+        return () => {
+            return { suit: this.suits[1], card: 2 };
+        };
+    },
+};
+
 function main() {
     assert(a.a == 1);
+
+    let cardPicker = deck.createCardPicker();
+    let pickedCard = cardPicker();
+
+    assert(pickedCard.card == 2);
+    assert(pickedCard.suit == "spades");
 
     print("done.");
 }
