a7762f9cb626ae695c1a4ddc409ace124387524a
AlexD <duzhar@googlemail.com>
2021-12-05 19:16:07 +0000

    added test

---
+++ tsc/test/tester/tests/01union_type.ts
@@ -0,0 +1,21 @@
+function main() {
+    let a: number | string;
+
+    a = "Hello";
+
+    if (typeof(a) == "string")
+    {
+	print("str val:", a);
+	assert (a == "Hello");
+    }
+
+    a = 10.0;
+
+    if (typeof(a) == "number")
+    {
+	print("num val:", a);
+	assert (a == 10.0);
+    }
+
+    print("done")
+}
