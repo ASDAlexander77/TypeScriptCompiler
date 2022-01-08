87494745c3752775cab26a46956afc7dbbde95e8
AlexD <duzhar@googlemail.com>
2022-01-08 13:54:35 +0000

    prepared test

---
+++ tsc/test/tester/tests/23generics.ts
@@ -0,0 +1,21 @@
+// TODO: not finished - testGenRef is not compiled (ignored)
+function testGenRef<T>(v: T) {
+    let x = v
+    // test that clear() also gets generalized
+    function clear() {
+        x = null
+    }
+    clear()
+}
+
+function testGenRefOuter() {
+    print("testGenRefOuter");
+    testGenRef(12)
+    testGenRef("fXa" + "baa")
+}
+
+function main()
+{
+    testGenRefOuter()
+    print("done.")
+}
