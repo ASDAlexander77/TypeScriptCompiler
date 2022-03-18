bec0b5aba66cdef96ffd743386e97b9faa7ce23b
AlexD <duzhar@googlemail.com>
2022-03-18 16:11:06 +0000

    implemented generic extents

---
+++ tsc/test/tester/tests/01funcs_generic.ts
@@ -0,0 +1,17 @@
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    //return obj[key]; // Inferred type is T[K]
+    return 1.0;
+}
+
+function setProperty<T, K extends keyof T>(obj: T, key: K, value: T[K]) {
+    //obj[key] = value;
+}
+
+function main() {
+
+    let x = { foo: 10, bar: "hello!" };
+    let foo = getProperty(x, "foo"); // number
+    let bar = getProperty(x, "bar"); // string
+
+    print("done.");
+}
