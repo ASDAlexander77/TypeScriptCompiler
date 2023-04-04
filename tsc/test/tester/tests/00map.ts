80958100965081849d8b9ab072260e2fe69a137b
ODuzhar <oduzhar@sportingindex.com>
2023-04-04 15:37:53 +0100

    update

--- tsc/test/tester/tests/00map.ts
+++ tsc/test/tester/tests/00map.ts
@@ -3,5 +3,8 @@ function main() {
 
     for (const v of arr.map(x => x + 1)) print(v);
 
+    let arrS = ["asd", "asd2"];
+    for (const e of arrS.map((e) => e + "_")) print(e);
+
     print("done.");
 }
