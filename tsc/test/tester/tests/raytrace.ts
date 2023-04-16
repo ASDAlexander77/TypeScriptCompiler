ff3bb72ab2004caa6417b1f0ac65ef030381dea9
ASDAlexander77 <duzhar@googlemail.com>
2023-04-16 19:04:40 +0100

    updated test for receiving type

--- tsc/test/tester/tests/raytrace.ts
+++ tsc/test/tester/tests/raytrace.ts
@@ -129,7 +129,7 @@ class Sphere implements Thing {
         if (dist === 0) {
             return null;
         } else {
-            return { thing: <Thing>this, ray: ray, dist: <number>dist };
+            return { thing: this, ray: ray, dist: dist };
         }
     }
 }
@@ -144,7 +144,7 @@ class Plane implements Thing {
             return null;
         } else {
             let dist = (Vector.dot(this.norm, ray.start) + this.offset) / (-denom);
-            return { thing: <Thing>this, ray: ray, dist: <number>dist };
+            return { thing: this, ray: ray, dist: dist };
         }
     }
 }
@@ -276,16 +276,16 @@ class RayTracer {
 
 function defaultScene(): Scene {
     return {
-        things: [<Thing>new Plane(new Vector(0.0, 1.0, 0.0), 0.0, Surfaces.checkerboard),
-        <Thing>new Sphere(new Vector(0.0, 1.0, -0.25), 1.0, Surfaces.shiny),
-        <Thing>new Sphere(new Vector(-6, 3.0, -5.0), 1.0, Surfaces.shiny),
+        things: [new Plane(new Vector(0.0, 1.0, 0.0), 0.0, Surfaces.checkerboard),
+        new Sphere(new Vector(0.0, 1.0, -0.25), 1.0, Surfaces.shiny),
+        new Sphere(new Vector(-6, 3.0, -5.0), 1.0, Surfaces.shiny),
         // new Sphere(new Vector(-6, 8.0, 15.0), 5.0, Surfaces.shiny),
-        <Thing>new Sphere(new Vector(-1.0, 0.5, 1.5), 0.5, Surfaces.shiny)],
+        new Sphere(new Vector(-1.0, 0.5, 1.5), 0.5, Surfaces.shiny)],
 
-        lights: [<Light>{ pos: new Vector(-2.0, 2.5, 0.0), color: new Color(0.49, 0.07, 0.07) },
-        <Light>{ pos: new Vector(1.5, 2.5, 1.5), color: new Color(0.07, 0.07, 0.49) },
-        <Light>{ pos: new Vector(1.5, 2.5, -1.5), color: new Color(0.07, 0.49, 0.071) },
-        <Light>{ pos: new Vector(0.0, 3.5, 0.0), color: new Color(0.21, 0.21, 0.35) }],
+        lights: [{ pos: new Vector(-2.0, 2.5, 0.0), color: new Color(0.49, 0.07, 0.07) },
+        { pos: new Vector(1.5, 2.5, 1.5), color: new Color(0.07, 0.07, 0.49) },
+        { pos: new Vector(1.5, 2.5, -1.5), color: new Color(0.07, 0.49, 0.071) },
+        { pos: new Vector(0.0, 3.5, 0.0), color: new Color(0.21, 0.21, 0.35) }],
         camera: new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0))
     };
 }
