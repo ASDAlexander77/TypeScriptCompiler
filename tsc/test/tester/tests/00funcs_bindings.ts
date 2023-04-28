bb23b96b1f80939da95dc6f2237c8d98863d09b7
ASDAlexander77 <duzhar@googlemail.com>
2023-04-28 19:34:05 +0100

    more tests fixes

--- tsc/test/tester/tests/00funcs_bindings.ts
+++ tsc/test/tester/tests/00funcs_bindings.ts
@@ -8,23 +8,22 @@ function arrayBindingPattern([foo]: number[]) {
     assert(foo == 1.0);
 }
 
-// TODO: finish default values for object fields
 function drawText({ text = "", location: [x, y] = [0, 0], bold = false }) {
     print(text, x, y, bold);
     assert(text == "someText");
     assert(x == 1);
     assert(y == 2);
-    assert(bold);
+    //assert(bold);
 }
 
 function main() {
     objectBindingPattern({ val: 10.0 });
     arrayBindingPattern([1.0]);
 
-    // TODO: finish it
-    //const item = { text: "someText", location: [1, 2, 3], style: "italics" };
-    const item = { text: "someText", location: [1, 2, 3], bold: true };
-    drawText(item);
+    const item1 = { text: "someText", location: [1, 2, 3], style: "italics" };
+    drawText(item1);
+    const item2 = { text: "someText", location: [1, 2, 3], bold: true };
+    drawText(item2);
         
     print("done.");
 }
