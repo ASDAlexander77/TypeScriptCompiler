68c4ed5821b694337a4253b6986a6fb0a3517a08
ASDAlexander77 <duzhar@googlemail.com>
2023-05-03 14:20:22 +0100

    added 1 more test

---
+++ tsc/test/tester/tests/00array7.ts
@@ -0,0 +1,19 @@
+function foo3(results: number[] | undefined) {
+    results ||= [];
+    if (results) print("has values");
+    let a = <number[]>results;
+    a.push(1);
+}
+
+function foo4(results: number[]) {
+    results ??= [];
+    if (results) print("has values");
+    results.push(1);
+}
+
+function main()
+{
+	foo3(undefined);
+	foo4(null);
+	print("done.");
+}
