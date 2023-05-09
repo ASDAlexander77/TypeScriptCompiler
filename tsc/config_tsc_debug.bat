4c5bb095e760f1796779fc2d232c1f566a3e0acf
ASDAlexander77 <duzhar@googlemail.com>
2023-05-09 14:40:38 +0100

    fixed issue with providing version

--- tsc/config_tsc_debug.bat
+++ tsc/config_tsc_debug.bat
@@ -2,5 +2,5 @@ pushd
 mkdir "../__build/tsc"
 cd "../__build/tsc"
 if exist "C:/Program Files/Microsoft Visual Studio/2022/Professional" set EXTRA_PARAM=-DCMAKE_GENERATOR_INSTANCE="C:/Program Files/Microsoft Visual Studio/2022/Professional"
-cmake ../../tsc -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Debug -Wno-dev %EXTRA_PARAM%
+cmake ../../tsc -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Debug -DTSC_PACKAGE_VERSION:STRING=1.2.3 -Wno-dev %EXTRA_PARAM%
 popd
