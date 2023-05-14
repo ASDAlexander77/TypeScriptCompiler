fa88ab1d019900ad56fb2314323b5052667388dd
ASDAlexander77 <duzhar@googlemail.com>
2023-05-14 11:49:19 +0100

    updated compile scripts

--- docs/MLIR_to_exe/via_llc_gc_async_d.bat
+++ docs/MLIR_to_exe/via_llc_gc_async_d.bat
@@ -2,7 +2,7 @@ call clean.bat
 call config_debug.bat
 %TSCEXEPATH%\tsc.exe --emit=llvm C:\temp\%FILENAME%.ts 2>%FILENAME%.ll
 %LLVMPATH%\llc.exe --filetype=obj -o=%FILENAME%.o %FILENAME%.ll
-%LLVMPATH%\lld.exe -flavor link %FILENAME%.o /libpath:%LIBPATH% /libpath:%SDKPATH% /libpath:%UCRTPATH% /libpath:%LLVMLIBPATH% /libpath:%GCLIBPATH% /libpath:%TSCLIBPATH% msvcrtd.lib ucrtd.lib kernel32.lib user32.lib gcmt-lib.lib TypeScriptAsyncRuntime.lib LLVMSupport.lib
+%LLVMPATH%\lld.exe -flavor link %FILENAME%.o /libpath:%LIBPATH% /libpath:%SDKPATH% /libpath:%UCRTPATH% /libpath:%LLVMLIBPATH% /libpath:%GCLIBPATH% /libpath:%TSCLIBPATH% msvcrtd.lib ucrtd.lib kernel32.lib user32.lib gcmt-lib.lib TypeScriptAsyncRuntime.lib LLVMSupport.lib LLVMDemangle.lib
 del %FILENAME%.o
 
 echo "RUN:..."
--- docs/MLIR_to_exe/via_llc_gc_async_opt.bat
+++ docs/MLIR_to_exe/via_llc_gc_async_opt.bat
@@ -2,7 +2,7 @@ call clean.bat
 call config_release.bat
 %TSCEXEPATH%\tsc.exe --opt --emit=llvm C:\temp\%FILENAME%.ts 2>%FILENAME%.ll
 %LLVMPATH%\llc.exe -O3 --filetype=obj -o=%FILENAME%.o %FILENAME%.ll
-%LLVMPATH%\lld.exe -flavor link %FILENAME%.o /libpath:%LIBPATH% /libpath:%SDKPATH% /libpath:%UCRTPATH% /libpath:%LLVMLIBPATH% /libpath:%GCLIBPATH% /libpath:%TSCLIBPATH% msvcrt.lib ucrt.lib kernel32.lib user32.lib gcmt-lib.lib TypeScriptAsyncRuntime.lib LLVMSupport.lib
+%LLVMPATH%\lld.exe -flavor link %FILENAME%.o /libpath:%LIBPATH% /libpath:%SDKPATH% /libpath:%UCRTPATH% /libpath:%LLVMLIBPATH% /libpath:%GCLIBPATH% /libpath:%TSCLIBPATH% msvcrt.lib ucrt.lib kernel32.lib user32.lib gcmt-lib.lib TypeScriptAsyncRuntime.lib LLVMSupport.lib LLVMDemangle.lib
 del %FILENAME%.o
 
 echo "RUN:..."
