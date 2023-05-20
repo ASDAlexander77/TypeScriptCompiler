d8eeceed16f4c35ba7a054026a1d201258599b08
ASDAlexander77 <duzhar@googlemail.com>
2023-05-20 18:56:56 +0100

    fix for global op with new array

--- tsc/lib/TypeScript/LowerToLLVM.cpp
+++ tsc/lib/TypeScript/LowerToLLVM.cpp
@@ -2657,10 +2657,19 @@ struct GlobalOpLowering : public TsLlvmPattern<mlir_ts::GlobalOp>
         auto visitorAllOps = [&](Operation *op) {
             if (isa<mlir_ts::NewOp>(op) || isa<mlir_ts::NewInterfaceOp>(op) || isa<mlir_ts::NewArrayOp>(op) ||
                 isa<mlir_ts::SymbolCallInternalOp>(op) || isa<mlir_ts::CallInternalOp>(op) ||
-                isa<mlir_ts::CallHybridInternalOp>(op) || isa<mlir_ts::VariableOp>(op) || isa<mlir_ts::AllocaOp>(op))
+                isa<mlir_ts::CallHybridInternalOp>(op) || isa<mlir_ts::VariableOp>(op) || isa<mlir_ts::AllocaOp>(op) ||
+                isa<mlir_ts::CreateArrayOp>(op))
             {
                 createAsGlobalConstructor = true;
             }
+
+            // TODO: error in try-catch
+            // if it has memory side effect - can't be in global init.
+            // auto iface = dyn_cast<MemoryEffectOpInterface>(op);
+            // if (!iface || !iface.hasNoEffect())
+            // {
+            //     createAsGlobalConstructor = true;
+            // }
         };
 
         globalOp.getInitializerRegion().walk(visitorAllOps);
