f2b4c5a58d68af55bfb3999dd17f1725250d7910
ASDAlexander77 <duzhar@googlemail.com>
2023-06-02 17:40:38 +0100

    progress of implementing obj dump

--- tsc/tsc/tsc.cpp
+++ tsc/tsc/tsc.cpp
@@ -84,6 +84,19 @@
 #include "llvm/CodeGen/LinkAllCodegenComponents.h"
 #endif
 
+// for dump obj
+#include "llvm/ADT/Triple.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/CodeGen/CommandFlags.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Target/TargetLoweringObjectFile.h"
+#include "llvm/IR/DiagnosticInfo.h"
+#include "llvm/IR/DiagnosticPrinter.h"
+
+// end of dump obj
+
 #define DEBUG_TYPE "tsc"
 
 #define ENABLE_CUSTOM_PASSES 1
@@ -94,6 +107,9 @@
 
 using namespace typescript;
 namespace cl = llvm::cl;
+
+// obj
+static llvm::codegen::RegisterCodeGenFlags CGF;
 
 cl::OptionCategory TypeScriptCompilerCategory("Compiler Options");
 cl::OptionCategory TypeScriptCompilerDebugCategory("Debug Options");
@@ -112,6 +128,7 @@ enum Action
     DumpMLIRLLVM,
     DumpLLVMIR,
     DumpByteCode,
+    DumpObj,
     RunJIT
 };
 } // namespace
@@ -123,6 +140,7 @@ static cl::opt<enum Action> emitAction("emit", cl::desc("Select the kind of outp
                                        cl::values(clEnumValN(DumpMLIRLLVM, "mlir-llvm", "output MLIR dump after llvm lowering")),
                                        cl::values(clEnumValN(DumpLLVMIR, "llvm", "output LLVM IR dump")),
                                        cl::values(clEnumValN(DumpByteCode, "bc", "output LLVM ByteCode dump")),
+                                       cl::values(clEnumValN(DumpObj, "obj", "output .OBJ file")),
                                        cl::values(clEnumValN(RunJIT, "jit", "JIT code and run it by invoking main function")), 
                                        cl::cat(TypeScriptCompilerCategory));
 
@@ -275,7 +293,7 @@ int dumpAST()
     return 0;
 }
 
-int initDialects(mlir::ModuleOp module)
+int registerMLIRDialects(mlir::ModuleOp module)
 {
     // Register the translation to LLVM IR with the MLIR context.
     mlir::registerLLVMDialectTranslation(*module->getContext());
@@ -321,8 +339,25 @@ static llvm::Optional<llvm::OptimizationLevel> mapToLevel(unsigned optLevel, uns
     return std::nullopt;
 }
 
+static char mapToLevel(unsigned optLevel)
+{
+    switch (optLevel)
+    {
+    case 0:
+        return '0';
+    case 1:
+        return '1';
+    case 2:
+        return '2';
+    case 3:
+        return '3';
+    }
 
-static std::unique_ptr<llvm::ToolOutputFile> GetOutputStream()
+    return '0';
+}
+
+
+static std::unique_ptr<llvm::ToolOutputFile> getOutputStream()
 {
     // If we don't yet have an output filename, make one.
     if (outputFilename.empty())
@@ -473,7 +508,7 @@ std::function<llvm::Error(llvm::Module *)> getTransformer(bool enableOpt, int op
 
 int dumpLLVMIR(mlir::ModuleOp module)
 {
-    initDialects(module);
+    registerMLIRDialects(module);
 
     // Convert the module to LLVM IR in a new LLVM IR context.
     llvm::LLVMContext llvmContext;
@@ -501,7 +536,7 @@ int dumpLLVMIR(mlir::ModuleOp module)
     if (emitAction == Action::DumpLLVMIR)
     {
         // TODO: add output into file as well 
-        auto FDOut = GetOutputStream();
+        auto FDOut = getOutputStream();
         if (FDOut)
         {
             FDOut->os() << *llvmModule << "\n";
@@ -515,7 +550,7 @@ int dumpLLVMIR(mlir::ModuleOp module)
 
     if (emitAction == Action::DumpByteCode)
     {
-        auto FDOut = GetOutputStream();
+        auto FDOut = getOutputStream();
         if (FDOut)
         {
             llvm::WriteBitcodeToFile(*llvmModule, FDOut->os());
@@ -532,6 +567,213 @@ int dumpLLVMIR(mlir::ModuleOp module)
     return 0;
 }
 
+struct LLCDiagnosticHandler : public llvm::DiagnosticHandler {
+  bool *HasError;
+  LLCDiagnosticHandler(bool *HasErrorPtr) : HasError(HasErrorPtr) {}
+  bool handleDiagnostics(const llvm::DiagnosticInfo &DI) override {
+    if (DI.getKind() == llvm::DK_SrcMgr) {
+      const auto &DISM = llvm::cast<llvm::DiagnosticInfoSrcMgr>(DI);
+      const llvm::SMDiagnostic &SMD = DISM.getSMDiag();
+
+      if (SMD.getKind() == llvm::SourceMgr::DK_Error)
+        *HasError = true;
+
+      SMD.print(nullptr, llvm::errs());
+
+      // For testing purposes, we print the LocCookie here.
+      if (DISM.isInlineAsmDiag() && DISM.getLocCookie())
+        llvm::WithColor::note() << "!srcloc = " << DISM.getLocCookie() << "\n";
+
+      return true;
+    }
+
+    if (DI.getSeverity() == llvm::DS_Error)
+      *HasError = true;
+
+    if (auto *Remark = llvm::dyn_cast<llvm::DiagnosticInfoOptimizationBase>(&DI))
+      if (!Remark->isEnabled())
+        return true;
+
+    llvm::DiagnosticPrinterRawOStream DP(llvm::errs());
+    llvm::errs() << llvm::LLVMContext::getDiagnosticMessagePrefix(DI.getSeverity()) << ": ";
+    DI.print(DP);
+    llvm::errs() << "\n";
+    return true;
+  }
+};
+
+int dumpObj(mlir::ModuleOp module)
+{
+    registerMLIRDialects(module);
+
+    // Convert the module to LLVM IR in a new LLVM IR context.
+    llvm::LLVMContext llvmContext;
+    auto llvmModule = mlir::translateModuleToLLVMIR(module, llvmContext);
+    if (!llvmModule)
+    {
+        llvm::WithColor::error(llvm::errs(), "tsc") << "Failed to emit LLVM IR\n";
+        return -1;
+    }
+
+    // Initialize LLVM targets.
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    mlir::ExecutionEngine::setupTargetTriple(llvmModule.get());
+
+    auto optPipeline = getTransformer(enableOpt, optLevel, sizeLevel);
+    if (auto err = optPipeline(llvmModule.get()))
+    {
+        llvm::WithColor::error(llvm::errs(), "tsc") << "Failed to optimize LLVM IR " << err << "\n";
+        return -1;
+    }
+
+    //
+    // generate Obj
+    //
+
+    llvm::LLVMContext Context;
+    // set from command line opt
+    //Context.setDiscardValueNames(DiscardValueNames);
+
+    // Set a diagnostic handler that doesn't exit on the first error
+    bool HasError = false;
+    Context.setDiagnosticHandler(std::make_unique<LLCDiagnosticHandler>(&HasError));
+
+
+    llvm::TargetOptions Options;
+    auto InitializeOptions = [&](const llvm::Triple &TheTriple) {
+        Options = llvm::codegen::InitTargetOptionsFromCodeGenFlags(TheTriple);
+        // Options.BinutilsVersion = llvm::TargetMachine::parseBinutilsVersion(BinutilsVersion);
+        // Options.DisableIntegratedAS = NoIntegratedAssembler;
+        // Options.MCOptions.ShowMCEncoding = ShowMCEncoding;
+        // Options.MCOptions.AsmVerbose = AsmVerbose;
+        // Options.MCOptions.PreserveAsmComments = PreserveComments;
+        // Options.MCOptions.IASSearchPaths = IncludeDirs;
+        // Options.MCOptions.SplitDwarfFile = SplitDwarfFile;
+        // if (DwarfDirectory.getPosition()) 
+        // {
+        //     Options.MCOptions.MCUseDwarfDirectory =
+        //     DwarfDirectory ? llvm::MCTargetOptions::EnableDwarfDirectory
+        //                    : llvm::MCTargetOptions::DisableDwarfDirectory;
+        // } else {
+        //     // -dwarf-directory is not set explicitly. Some assemblers
+        //     // (e.g. GNU as or ptxas) do not support `.file directory'
+        //     // syntax prior to DWARFv5. Let the target decide the default
+        //     // value.
+        //     Options.MCOptions.MCUseDwarfDirectory = MCTargetOptions::DefaultDwarfDirectory;
+        // }
+    };
+
+    std::optional<llvm::Reloc::Model> RM = llvm::codegen::getExplicitRelocModel();
+    std::optional<llvm::CodeModel::Model> CM = llvm::codegen::getExplicitCodeModel();
+
+    llvm::Triple TheTriple;
+    const llvm::Target *TheTarget = nullptr;
+    std::unique_ptr<llvm::TargetMachine> Target;
+    
+    // If we are supposed to override the target triple, do so now.
+    std::string LLVMModuleTargetTriple = llvmModule.get()->getTargetTriple();
+    /*
+    // override from command line here
+    */
+    
+    TheTriple = llvm::Triple(LLVMModuleTargetTriple);
+    if (TheTriple.getTriple().empty())
+    {
+        TheTriple.setTriple(llvm::sys::getDefaultTargetTriple());
+    }
+
+    std::string Error;
+    TheTarget = llvm::TargetRegistry::lookupTarget(llvm::codegen::getMArch(), TheTriple, Error);
+    if (!TheTarget) 
+    {
+        llvm::WithColor::error(llvm::errs(), "tsc") << Error;
+        return -1;
+    }
+
+      // On AIX, setting the relocation model to anything other than PIC is
+      // considered a user error.
+    if (TheTriple.isOSAIX() && RM && *RM != llvm::Reloc::PIC_)
+    {
+        llvm::WithColor::error(llvm::errs(), "tsc") << "invalid relocation model, AIX only supports PIC";
+        return -1;
+    }
+
+    auto CPUStr = llvm::codegen::getCPUStr(), 
+         FeaturesStr = llvm::codegen::getFeaturesStr();
+
+    llvm::CodeGenOpt::Level OLvl;
+    // TODO: finish it, optimization
+    if (auto Level = llvm::CodeGenOpt::parseLevel(enableOpt ? mapToLevel(optLevel) : '0')) 
+    {
+        OLvl = *Level;
+    } 
+    else 
+    {
+        llvm::WithColor::error(llvm::errs(), "tsc") << "invalid optimization level.\n";
+        return 1;
+    }
+
+
+    InitializeOptions(TheTriple);
+    Target = std::unique_ptr<llvm::TargetMachine>(TheTarget->createTargetMachine(
+          TheTriple.getTriple(), CPUStr, FeaturesStr, Options, RM, CM, OLvl));
+    assert(Target && "Could not allocate target machine!");
+
+    // Build up all of the passes that we want to do to the module.
+    llvm::legacy::PassManager PM;
+
+    // Add an appropriate TargetLibraryInfo pass for the module's triple.
+    llvm::TargetLibraryInfoImpl TLII(llvm::Triple(llvmModule->getTargetTriple()));
+
+    PM.add(new llvm::TargetLibraryInfoWrapperPass(TLII));
+
+    // TODO: disable it in release
+    if (llvm::verifyModule(*llvmModule.get(), &llvm::errs()))
+    {
+        llvm::WithColor::error(llvm::errs(), "tsc") << "input module cannot be verified\n";
+        return -1;        
+    }
+
+    // Override function attributes based on CPUStr, FeaturesStr, and command line flags.
+    llvm::codegen::setFunctionAttributes(CPUStr, FeaturesStr, *llvmModule.get());
+
+    {
+        auto FDOut = getOutputStream();
+
+        auto &LLVMTM = static_cast<llvm::LLVMTargetMachine &>(*Target);
+        auto *MMIWP = new llvm::MachineModuleInfoWrapperPass(&LLVMTM);
+
+        if (Target->addPassesToEmitFile(
+                        PM, FDOut->os(), /*DwoOut ? &DwoOut->os() : */nullptr,
+                        llvm::codegen::getFileType(), true, MMIWP)) 
+        {
+            llvm::WithColor::error(llvm::errs(), "tsc") << "target does not support generation of this file type\n";
+        }
+
+        const_cast<llvm::TargetLoweringObjectFile *>(LLVMTM.getObjFileLowering())->Initialize(MMIWP->getMMI().getContext(), *Target);
+
+        // Before executing passes, print the final values of the LLVM options.
+        llvm::cl::PrintOptionValues();
+
+        PM.run(*llvmModule.get());
+
+        auto HasError = ((const LLCDiagnosticHandler *)(Context.getDiagHandlerPtr()))->HasError;
+        if (*HasError)
+        {
+            llvm::WithColor::error(llvm::errs(), "tsc") << "diagnostic error(s)\n";
+            return 1;
+        }
+
+        // Declare success.
+        FDOut->keep();
+
+        return 0;
+    }
+
+    return 0;
+}
+
 int runJit(int argc, char **argv, mlir::ModuleOp module)
 {
     // Print a stack trace if we signal out.
@@ -541,7 +783,7 @@ int runJit(int argc, char **argv, mlir::ModuleOp module)
 
     llvm::llvm_shutdown_obj Y; // Call llvm_shutdown() on exit.
 
-    initDialects(module);
+    registerMLIRDialects(module);
 
     // Initialize LLVM targets.
     llvm::InitializeNativeTarget();
@@ -771,6 +1013,11 @@ int main(int argc, char **argv)
     if (emitAction == Action::DumpLLVMIR || emitAction == Action::DumpByteCode)
     {
         return dumpLLVMIR(*module);
+    }
+
+    if (emitAction == Action::DumpObj)
+    {
+        return dumpObj(*module);
     }
 
     // Otherwise, we must be running the jit.
