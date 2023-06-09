f7308a2653d56d20a6671d4d8bd97956745945f5
ASDAlexander77 <duzhar@googlemail.com>
2023-06-09 16:43:04 +0100

    sync more files

--- tsc/config_tsc_release.bat
+++ tsc/config_tsc_release.bat
@@ -2,5 +2,5 @@ pushd
 mkdir "../__build/tsc/msbuild/x64/release"
 cd "../__build/tsc/msbuild/x64/release"
 if exist "C:/Program Files/Microsoft Visual Studio/2022/Professional" set EXTRA_PARAM=-DCMAKE_GENERATOR_INSTANCE="C:/Program Files/Microsoft Visual Studio/2022/Professional"
-cmake ../../tsc -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -Wno-dev %EXTRA_PARAM%
+cmake ../../../../../tsc -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -Wno-dev %EXTRA_PARAM%
 popd
