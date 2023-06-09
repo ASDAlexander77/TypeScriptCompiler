cf837b0311e4da1cf47739b3b36d7baf62f66fa7
ASDAlexander77 <duzhar@googlemail.com>
2023-06-09 17:56:48 +0100

    sync files

--- scripts/build_gc_debug.sh
+++ scripts/build_gc_debug.sh
@@ -3,5 +3,5 @@ mkdir -p __build/gc/ninja/debug
 mkdir -p 3rdParty/gc/debug
 cd __build/gc/ninja/debug
 cmake ../../../../3rdParty/gc-8.0.4 -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=../../../../3rdParty/gc/debug -Denable_threads=ON -Denable_cplusplus=OFF
-cmake --build . --config Debug -j 8
+cmake --build . --config Debug -j 16
 cp ./lib* ../../../../3rdParty/gc/debug/
