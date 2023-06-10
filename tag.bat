b79f1ca135a24d6bacfc669ac7d200d900f9582c
ASDAlexander77 <duzhar@googlemail.com>
2023-06-10 19:35:03 +0100

    update

--- tag.bat
+++ tag.bat
@@ -1,2 +1,2 @@
-git tag -a v0.0-pre-alpha35 -m "pre alpha v0.0-35"
+git tag -a v0.0-pre-alpha36 -m "pre alpha v0.0-36"
 git push origin --tags
--- tag_del.bat
+++ tag_del.bat
@@ -1,3 +1,3 @@
-git push --delete origin v0.0-pre-alpha35
-git tag -d v0.0-pre-alpha35
+git push --delete origin v0.0-pre-alpha36
+git tag -d v0.0-pre-alpha36
 
