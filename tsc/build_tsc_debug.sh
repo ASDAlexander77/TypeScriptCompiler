9f464b11e7518b4430803ebe4e79d1ad119ffbe3
ASDAlexander77 <duzhar@googlemail.com>
2023-07-08 13:28:51 +0100

    fixes to build files

--- tsc/build_tsc_debug.sh
+++ tsc/build_tsc_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../__build/tsc/ninja/debug
 cmake --build . --config Debug -j 8
-bash -f ../../scripts/separate_debug_info.sh ./bin/tsc
+bash -f ../../../../scripts/separate_debug_info.sh ./bin/tsc
