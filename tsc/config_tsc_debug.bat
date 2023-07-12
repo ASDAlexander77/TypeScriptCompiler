c586a6b9467d3f9310df87e2c9bdede2d7d07554
ASDAlexander77 <duzhar@googlemail.com>
2023-07-12 16:26:09 +0100

    added unit tests

--- tsc/config_tsc_debug.bat
+++ tsc/config_tsc_debug.bat
@@ -2,5 +2,5 @@ pushd
 mkdir "../__build/tsc/msbuild/x64/debug"
 cd "../__build/tsc/msbuild/x64/debug"
 if exist "C:/Program Files/Microsoft Visual Studio/2022/Professional" set EXTRA_PARAM=-DCMAKE_GENERATOR_INSTANCE="C:/Program Files/Microsoft Visual Studio/2022/Professional"
-cmake ../../../../../tsc -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Debug -DTSC_PACKAGE_VERSION:STRING=1.2.3 -Wno-dev %EXTRA_PARAM%
+cmake ../../../../../tsc -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Debug -DTSC_PACKAGE_VERSION:STRING=1.2.3 -DTYPESCRIPT_INCLUDE_TESTS=any -Wno-dev %EXTRA_PARAM%
 popd
