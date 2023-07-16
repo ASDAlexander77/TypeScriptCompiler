c7fa1d35b8c2494950895f50b34f12bc2dbdc20c
ASDAlexander77 <duzhar@googlemail.com>
2023-07-17 00:27:07 +0100

    progress of adding test for new printer

--- tsc/test/print-tester/printer-tester.cpp
+++ tsc/test/print-tester/printer-tester.cpp
@@ -10,11 +10,36 @@
 
 using namespace ts;
 
+void getNodes(ts::SourceFile sourceFile, NodeArray<Node> &nodes)
+{
+    FuncT<> visitNode;
+    ArrayFuncT<> visitArray;
+
+    visitNode = [&](Node child) -> Node {
+
+        nodes.push_back(child);
+
+        ts::forEachChild(child, visitNode, visitArray);
+        return undefined;
+    };
+
+    visitArray = [&](NodeArray<Node> array) -> Node {
+        for (auto node : array)
+        {
+            visitNode(node);
+        }
+
+        return undefined;
+    };
+
+    forEachChild(sourceFile.as<Node>(), visitNode, visitArray);
+}
+
 int main(int argc, char **argv)
 {
     if (argc <= 1)
     {
-        puts("Usage: print-tester <file path>.ts");
+        puts("Usage: print-tester <file path>.ts [compare]?");
         return 0;
     }
 
@@ -30,7 +55,63 @@ int main(int argc, char **argv)
     //printer.setDeclarationMode(true);
     printer.printNode(sourceFile);
 
-    puts(wstos(out.str()).c_str());
+    auto newContent = out.str();
+    if (argc <= 2)
+    {
+        puts(wstos(newContent).c_str());
+    }
+    else
+    {
+        // compare mode
+        auto newSourceFile = parser.parseSourceFile(stows(filePath), newContent, ScriptTarget::Latest);
+
+        NodeArray<Node> oldNodes;
+        NodeArray<Node> newNodes;
+
+        getNodes(sourceFile, oldNodes);
+        getNodes(newSourceFile, newNodes);
+
+        auto count = oldNodes.size() > newNodes.size() ? oldNodes.size() : newNodes.size();
+
+        if (count == 0)
+        {
+            puts(" : ERROR, count = 0");
+            exit(1);
+        }
+
+        if (oldNodes.size() != newNodes.size())
+        {
+            puts(" : not equal count.");
+        }
+
+        for (auto index = 0; index < count; index++)
+        {
+            auto oldNode = oldNodes[index];
+            auto newNode = newNodes[index];
+
+            // compare
+            if ((SyntaxKind)oldNode != (SyntaxKind)newNode)
+            {
+                auto posLineChar = parser.getLineAndCharacterOfPosition(sourceFile, oldNode->pos);
+                auto endLineChar = parser.getLineAndCharacterOfPosition(sourceFile, oldNode->_end);
+
+                stringstream s;
+                s << S("Node: ") << parser.syntaxKindString(oldNode).c_str() << S(" @ [ ") << oldNode->pos << S("(")
+                << posLineChar.line + 1 << S(":") << posLineChar.character + 1 << S(") - ") << oldNode->_end << S("(")
+                << endLineChar.line + 1 << S(":") << endLineChar.character << S(") ]")
+                << std::endl << " old text: " << content.substr(oldNode->pos, oldNode->_end - oldNode->pos)
+                << std::endl << " new text: " << newContent.substr(newNode->pos, newNode->_end - newNode->pos);
+
+                puts(wstos(s.str()).c_str());
+                puts(argv[1]);
+                puts(" : not equal.");
+                exit(1);
+            }
+        }
+
+        puts(argv[1]);
+        puts(" : equal.");
+    }
 
     return 0;
 }
