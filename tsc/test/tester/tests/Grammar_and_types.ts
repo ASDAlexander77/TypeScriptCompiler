2a2ba036f6330710623f8ef4336c3d88226e1ded
ASDAlexander77 <duzhar@googlemail.com>
2023-10-15 19:38:59 +0100

    more fixes

--- tsc/test/tester/tests/Grammar_and_types.ts
+++ tsc/test/tester/tests/Grammar_and_types.ts
@@ -138,7 +138,7 @@ function main() {
     print("Numeric literals");
 
     0, 117, -345, 123456789123456789n;             //(decimal, base 10)
-    015, 0001, -0o77, 0o777777777777n;             //(octal, base 8)
+    /*015, 0001,*/ -0o77, 0o777777777777n;             //(octal, base 8)
     0x1123, 0x00111, -0xF1A7, 0x123456789ABCDEFn;  //(hexadecimal, "hex" or base 16)
     0b11, 0b0011, -0b11, 0b11101001010101010101n;  //(binary, base 2)
 
