18b244607f7f6faadd80c760b3226d740a247002
ASDAlexander77 <duzhar@googlemail.com>
2023-11-19 13:13:09 +0000

    updated test

--- tsc/test/tester/tests/00try_finally_return.ts
+++ tsc/test/tester/tests/00try_finally_return.ts
@@ -1,30 +1,49 @@
 let called = false;
 
-function may_throw(a = 0)
-{
-	if (a > 100) throw 1;
+function may_throw(a = 0) {
+    if (a > 100) throw 1;
 }
 
-function func()
-{
-	try
-	{
-		print("In try");
-		may_throw(10);
-		return;
-	}
-	finally
-	{
-		print("finally");
+function func1() {
+    try {
+        print("In try");
+        may_throw(10);
+        return;
+    }
+    finally {
+        print("finally");
         called = true;
-	}
+    }
 
-	print("end");
+    called = false;
+    print("end");
 }
 
-function main()
-{
-	func();
+function func2(a = 0) {
+    try {
+        print("In try");
+        may_throw(1);
+        if (a > 20)
+            return;
+    }
+    catch (e: TypeOf<1>) {
+        print("catch");
+        throw e;
+    }
+    finally {
+        print("finally");
+        called = true;
+    }
+
+    called = false;
+    print("end");
+}
+
+function main() {
+    func1();
     assert(called, "finally is not called");
-	print("done.");
+    called = false;
+    func2(100);
+    assert(called, "finally is not called");
+    print("done.");
 }
