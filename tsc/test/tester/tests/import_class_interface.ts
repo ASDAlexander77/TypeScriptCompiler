5b54056ed6d9341e33738e96249579252a0a12d8
ASDAlexander77 <duzhar@googlemail.com>
2024-11-19 15:37:52 +0000

    added export/import test

---
+++ tsc/test/tester/tests/import_class_interface.ts
@@ -0,0 +1,14 @@
+import './export_class_interface'
+
+const p = new A.Point2d(1, 2);
+const v = 
+    p.fromOrigin({
+        x:1, 
+        y:2, 
+        fromOrigin(p: A.Point) { return 1.0; }   
+    });
+
+print(v);    
+
+print("done.");
+
