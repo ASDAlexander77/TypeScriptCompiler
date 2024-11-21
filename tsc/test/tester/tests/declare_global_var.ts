310d6133500e0e49e04cfca64df19968380a65be
ASDAlexander77 <duzhar@googlemail.com>
2024-11-21 18:52:35 +0000

    added test

---
+++ tsc/test/tester/tests/declare_global_var.ts
@@ -0,0 +1,4 @@
+declare static class Math {
+    /** Pi. This is the ratio of the circumference of a circle to its diameter. */
+    readonly PI: number;
+}
---
+++ tsc/test/tester/tests/define_global_var.ts
@@ -0,0 +1,4 @@
+export static class Math {
+    /** Pi. This is the ratio of the circumference of a circle to its diameter. */
+    public PI = 3.141592653589793;
+}
