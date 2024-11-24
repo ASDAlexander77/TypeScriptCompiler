492efa431a485da8bd38a83ec016375132bb3f64
ASDAlexander77 <duzhar@googlemail.com>
2024-11-24 16:46:05 +0000

    updated test

--- tsc/test/tester/tests/import_class_interface.ts
+++ tsc/test/tester/tests/import_class_interface.ts
@@ -1,16 +1,16 @@
 import './export_class_interface'
 
-const p = new A.Point2d(1, 2);
+const p = new A.Point2d(10, 20);
 const v = 
     p.fromOrigin({
-        x:120, 
-        y:3, 
-        fromOrigin(p: A.Point) { return 1.0; }   
+        x:100, 
+        y:200, 
+        fromOrigin(p: A.Point) { return p.x * p.y + this.x + this.y; }   
     });
 
 print(v);    
 
-assert(v == 126.0);
+assert(v == 830.0);
 
 print("done.");
 
