84346dd81cef66759798541ca559877f32e53e9e
ASDAlexander77 <duzhar@googlemail.com>
2024-12-21 13:13:52 +0000

    updated into R string

--- tsc/include/TypeScript/VSCodeTemplate/Files.h
+++ tsc/include/TypeScript/VSCodeTemplate/Files.h
@@ -1,8 +1,262 @@
 #define NODE_MODULE_TSNC_PATH "node_modules/tsnc"
 #define DOT_VSCODE_PATH ".vscode"
 
-#define TSCONFIG_JSON_DATA "{\r\n    \"compilerOptions\": {\r\n      \"target\": \"es2017\",\r\n      \"lib\": [\"dom\", \"dom.iterable\", \"esnext\"],\r\n      \"allowJs\": true,\r\n      \"skipLibCheck\": true,\r\n      \"strict\": true,\r\n      \"noEmit\": true,\r\n      \"esModuleInterop\": true,\r\n      \"module\": \"esnext\",\r\n      \"moduleResolution\": \"bundler\",\r\n      \"resolveJsonModule\": true,\r\n      \"isolatedModules\": true,\r\n      \"jsx\": \"preserve\",\r\n      \"incremental\": true,\r\n      \"types\": [\"tsnc\"]\r\n    },\r\n    \"include\": [\"<<PROJECT>>.ts\"],\r\n    \"exclude\": [\"node_modules\"]\r\n  }"
-#define TSNC_INDEX_D_TS "declare function print(...args: (string | number)[]) : void;\r\ndeclare function assert(cond: boolean, msg?: string) : void;\r\ndeclare type int = any;"
-#define TASKS_JSON_DATA "{\r\n    // See https://go.microsoft.com/fwlink/?LinkId=733558\r\n    // for the documentation about the tasks.json format\r\n    \"version\": \"2.0.0\",\r\n    \"tasks\": [\r\n        {\r\n            \"label\": \"build EXE (Debug)\",\r\n            \"command\": \"<<TSC_CMD>>\",\r\n            \"type\": \"shell\",\r\n            \"args\": [\r\n                \"--gc-lib-path=<<GC_LIB_PATH>>\",\r\n                \"--llvm-lib-path=<<LLVM_LIB_PATH>>\",\r\n                \"--tsc-lib-path=<<TSC_LIB_PATH>>\",\r\n                \"--default-lib-path=<<DEFAULT_LIB_PATH>>\",\r\n                \"--no-default-lib\",\r\n                \"--di\",\r\n                \"--opt_level=0\",\r\n                \"--emit=exe\",\r\n                \"${file}\"\r\n            ],\r\n            \"group\": {\r\n                \"kind\": \"build\",\r\n                \"isDefault\": true\r\n            },\r\n            \"presentation\": {\r\n                \"reveal\": \"silent\"\r\n            },\r\n            \"problemMatcher\": \"$msCompile\"\r\n        },\r\n        {\r\n            \"label\": \"build EXE (Release)\",\r\n            \"command\": \"<<TSC_CMD>>\",\r\n            \"type\": \"shell\",\r\n            \"args\": [\r\n                \"--gc-lib-path=<<GC_LIB_PATH>>\",\r\n                \"--llvm-lib-path=<<LLVM_LIB_PATH>>\",\r\n                \"--tsc-lib-path=<<TSC_LIB_PATH>>\",\r\n                \"--default-lib-path=<<DEFAULT_LIB_PATH>>\",\r\n                \"--no-default-lib\",\r\n                \"--opt\",\r\n                \"--opt_level=3\",\r\n                \"--emit=exe\",\r\n                \"${file}\"\r\n            ],\r\n            \"group\": {\r\n                \"kind\": \"build\",\r\n                \"isDefault\": false\r\n            },\r\n            \"presentation\": {\r\n                \"reveal\": \"silent\"\r\n            },\r\n            \"problemMatcher\": \"$msCompile\"\r\n        },\r\n        {\r\n            \"label\": \"build DLL (Debug)\",\r\n            \"command\": \"<<TSC_CMD>>\",\r\n            \"type\": \"shell\",\r\n            \"args\": [\r\n                \"--gc-lib-path=<<GC_LIB_PATH>>\",\r\n                \"--llvm-lib-path=<<LLVM_LIB_PATH>>\",\r\n                \"--tsc-lib-path=<<TSC_LIB_PATH>>\",\r\n                \"--default-lib-path=<<DEFAULT_LIB_PATH>>\",\r\n                \"--no-default-lib\",\r\n                \"--di\",\r\n                \"--opt_level=0\",\r\n                \"--emit=exe\",\r\n                \"${file}\"\r\n            ],\r\n            \"group\": {\r\n                \"kind\": \"build\",\r\n                \"isDefault\": false\r\n            },\r\n            \"presentation\": {\r\n                \"reveal\": \"silent\"\r\n            },\r\n            \"problemMatcher\": \"$msCompile\"\r\n        },\r\n        {\r\n            \"label\": \"build DLL (Release)\",\r\n            \"command\": \"<<TSC_CMD>>\",\r\n            \"type\": \"shell\",\r\n            \"args\": [\r\n                \"--gc-lib-path=<<GC_LIB_PATH>>\",\r\n                \"--llvm-lib-path=<<LLVM_LIB_PATH>>\",\r\n                \"--tsc-lib-path=<<TSC_LIB_PATH>>\",\r\n                \"--default-lib-path=<<DEFAULT_LIB_PATH>>\",\r\n                \"--no-default-lib\",\r\n                \"--opt\",\r\n                \"--opt_level=3\",\r\n                \"--emit=exe\",\r\n                \"${file}\"\r\n            ],\r\n            \"group\": {\r\n                \"kind\": \"build\",\r\n                \"isDefault\": false\r\n            },\r\n            \"presentation\": {\r\n                \"reveal\": \"silent\"\r\n            },\r\n            \"problemMatcher\": \"$msCompile\"\r\n        }        \r\n    ]\r\n}"
-#define LAUNCH_JSON_DATA_WIN32 "{\r\n    // Use IntelliSense to learn about possible attributes.\r\n    // Hover to view descriptions of existing attributes.\r\n    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387\r\n    \"version\": \"0.2.0\",\r\n    \"configurations\": [\r\n        {\r\n            \"name\": \"Current File - EXE (Debug)\",\r\n            \"type\": \"cppvsdbg\",\r\n            \"preLaunchTask\": \"build EXE (Debug)\",\r\n            \"request\": \"launch\",\r\n            \"program\": \"${fileBasenameNoExtension}.exe\",\r\n            \"args\": [\r\n            ],\r\n            \"stopAtEntry\": false,\r\n            \"cwd\": \"${workspaceFolder}\",\r\n            \"symbolSearchPath\": \"${workspaceFolder}\",\r\n            \"environment\": [\r\n            ],\r\n            \"visualizerFile\": \"${workspaceFolder}/tsnc.natvis\"\r\n        },         \r\n        {\r\n            \"name\": \"Current File - EXE (Release)\",\r\n            \"type\": \"cppvsdbg\",\r\n            \"preLaunchTask\": \"build EXE (Release)\",\r\n            \"request\": \"launch\",\r\n            \"program\": \"${fileBasenameNoExtension}.exe\",\r\n            \"args\": [\r\n            ],\r\n            \"stopAtEntry\": false,\r\n            \"cwd\": \"${workspaceFolder}\",\r\n            \"symbolSearchPath\": \"${workspaceFolder}\",\r\n            \"environment\": [\r\n            ],\r\n            \"visualizerFile\": \"${workspaceFolder}/tsnc.natvis\"\r\n        },         \r\n        {\r\n            \"name\": \"Current File - JIT\",\r\n            \"type\": \"cppvsdbg\",\r\n            \"request\": \"launch\",\r\n            \"program\": \"<<TSC_CMD>>\",\r\n            \"args\": [\r\n                \"--shared-libs=<<TSC_LIB_PATH>>\\\\TypeScriptRuntime.dll\",\r\n                \"--gc-lib-path=<<GC_LIB_PATH>>\",\r\n                \"--llvm-lib-path=<<LLVM_LIB_PATH>>\",\r\n                \"--tsc-lib-path=<<TSC_LIB_PATH>>\",\r\n                \"--default-lib-path=<<DEFAULT_LIB_PATH>>\",\r\n                \"--no-default-lib\",\r\n                \"--opt\",\r\n                \"--opt_level=3\",                \r\n                \"--emit=jit\",\r\n                \"${file}\"                \r\n            ],\r\n            \"stopAtEntry\": false,\r\n            \"cwd\": \"${workspaceFolder}\",\r\n            \"symbolSearchPath\": \"${workspaceFolder}\",\r\n            \"environment\": [\r\n            ]\r\n        }\r\n    ]\r\n}"
-#define LAUNCH_JSON_DATA_LINUX "{\r\n    // Use IntelliSense to learn about possible attributes.\r\n    // Hover to view descriptions of existing attributes.\r\n    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387\r\n    \"version\": \"0.2.0\",\r\n    \"configurations\": [\r\n        {\r\n            \"name\": \"Current File - EXE (Debug)\",\r\n            \"type\": \"cppdbg\",\r\n            \"preLaunchTask\": \"build EXE (Debug)\",\r\n            \"request\": \"launch\",\r\n            \"program\": \"${fileBasenameNoExtension}\",\r\n            \"args\": [\r\n            ],\r\n            \"stopAtEntry\": false,\r\n            \"cwd\": \"${workspaceFolder}\",\r\n            \"environment\": [\r\n            ],\r\n            \"visualizerFile\": \"${workspaceFolder}/tsnc.natvis\"\r\n        },         \r\n        {\r\n            \"name\": \"Current File - EXE (Release)\",\r\n            \"type\": \"cppdbg\",\r\n            \"preLaunchTask\": \"build EXE (Release)\",\r\n            \"request\": \"launch\",\r\n            \"program\": \"${fileBasenameNoExtension}\",\r\n            \"args\": [\r\n            ],\r\n            \"stopAtEntry\": false,\r\n            \"cwd\": \"${workspaceFolder}\",\r\n            \"environment\": [\r\n            ],\r\n            \"visualizerFile\": \"${workspaceFolder}/tsnc.natvis\"\r\n        },         \r\n        {\r\n            \"name\": \"Current File - JIT\",\r\n            \"type\": \"cppdbg\",\r\n            \"request\": \"launch\",\r\n            \"program\": \"<<TSC_CMD>>\",\r\n            \"args\": [\r\n                \"--shared-libs=<<TSC_LIB_PATH>>/libTypeScriptRuntime.so\",\r\n                \"--gc-lib-path=<<GC_LIB_PATH>>\",\r\n                \"--llvm-lib-path=<<LLVM_LIB_PATH>>\",\r\n                \"--tsc-lib-path=<<TSC_LIB_PATH>>\",\r\n                \"--default-lib-path=<<DEFAULT_LIB_PATH>>\",\r\n                \"--no-default-lib\",\r\n                \"--opt\",\r\n                \"--opt_level=3\",                \r\n                \"--emit=jit\",\r\n                \"${file}\"                \r\n            ],\r\n            \"stopAtEntry\": false,\r\n            \"cwd\": \"${workspaceFolder}\",\r\n            \"environment\": [\r\n            ]\r\n        }\r\n    ]\r\n}"
+#define TSCONFIG_JSON_DATA R"raw(
+{
+    "compilerOptions": {
+      "target": "es2017",
+      "lib": ["dom", "dom.iterable", "esnext"],
+      "allowJs": true,
+      "skipLibCheck": true,
+      "strict": true,
+      "noEmit": true,
+      "esModuleInterop": true,
+      "module": "esnext",
+      "moduleResolution": "bundler",
+      "resolveJsonModule": true,
+      "isolatedModules": true,
+      "jsx": "preserve",
+      "incremental": true,
+      "types": ["tsnc"]
+    },
+    "include": ["<<PROJECT>>.ts"],
+    "exclude": ["node_modules"]
+}
+)raw"
+
+#define TSNC_INDEX_D_TS R"raw(
+declare function print(...args: (string | number)[]) : void;
+declare function assert(cond: boolean, msg?: string) : void;
+declare type int = any;
+)raw"
+
+#define TASKS_JSON_DATA R"raw(
+{
+    // See https://go.microsoft.com/fwlink/?LinkId=733558
+    // for the documentation about the tasks.json format
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "label": "build EXE (Debug)",
+            "command": "<<TSC_CMD>>",
+            "type": "shell",
+            "args": [
+                "--gc-lib-path=<<GC_LIB_PATH>>",
+                "--llvm-lib-path=<<LLVM_LIB_PATH>>",
+                "--tsc-lib-path=<<TSC_LIB_PATH>>",
+                "--default-lib-path=<<DEFAULT_LIB_PATH>>",
+                "--no-default-lib",
+                "--di",
+                "--opt_level=0",
+                "--emit=exe",
+                "${file}"
+            ],
+            "group": {
+                "kind": "build",
+                "isDefault": true
+            },
+            "presentation": {
+                "reveal": "silent"
+            },
+            "problemMatcher": "$msCompile"
+        },
+        {
+            "label": "build EXE (Release)",
+            "command": "<<TSC_CMD>>",
+            "type": "shell",
+            "args": [
+                "--gc-lib-path=<<GC_LIB_PATH>>",
+                "--llvm-lib-path=<<LLVM_LIB_PATH>>",
+                "--tsc-lib-path=<<TSC_LIB_PATH>>",
+                "--default-lib-path=<<DEFAULT_LIB_PATH>>",
+                "--no-default-lib",
+                "--opt",
+                "--opt_level=3",
+                "--emit=exe",
+                "${file}"
+            ],
+            "group": {
+                "kind": "build",
+                "isDefault": false
+            },
+            "presentation": {
+                "reveal": "silent"
+            },
+            "problemMatcher": "$msCompile"
+        },
+        {
+            "label": "build DLL (Debug)",
+            "command": "<<TSC_CMD>>",
+            "type": "shell",
+            "args": [
+                "--gc-lib-path=<<GC_LIB_PATH>>",
+                "--llvm-lib-path=<<LLVM_LIB_PATH>>",
+                "--tsc-lib-path=<<TSC_LIB_PATH>>",
+                "--default-lib-path=<<DEFAULT_LIB_PATH>>",
+                "--no-default-lib",
+                "--di",
+                "--opt_level=0",
+                "--emit=exe",
+                "${file}"
+            ],
+            "group": {
+                "kind": "build",
+                "isDefault": false
+            },
+            "presentation": {
+                "reveal": "silent"
+            },
+            "problemMatcher": "$msCompile"
+        },
+        {
+            "label": "build DLL (Release)",
+            "command": "<<TSC_CMD>>",
+            "type": "shell",
+            "args": [
+                "--gc-lib-path=<<GC_LIB_PATH>>",
+                "--llvm-lib-path=<<LLVM_LIB_PATH>>",
+                "--tsc-lib-path=<<TSC_LIB_PATH>>",
+                "--default-lib-path=<<DEFAULT_LIB_PATH>>",
+                "--no-default-lib",
+                "--opt",
+                "--opt_level=3",
+                "--emit=exe",
+                "${file}"
+            ],
+            "group": {
+                "kind": "build",
+                "isDefault": false
+            },
+            "presentation": {
+                "reveal": "silent"
+            },
+            "problemMatcher": "$msCompile"
+        }        
+    ]
+}
+)raw"
+
+#define LAUNCH_JSON_DATA_WIN32 R"raw(
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Current File - EXE (Debug)",
+            "type": "cppvsdbg",
+            "preLaunchTask": "build EXE (Debug)",
+            "request": "launch",
+            "program": "${fileBasenameNoExtension}.exe",
+            "args": [
+            ],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "symbolSearchPath": "${workspaceFolder}",
+            "environment": [
+            ],
+            "visualizerFile": "${workspaceFolder}/tsnc.natvis"
+        },         
+        {
+            "name": "Current File - EXE (Release)",
+            "type": "cppvsdbg",
+            "preLaunchTask": "build EXE (Release)",
+            "request": "launch",
+            "program": "${fileBasenameNoExtension}.exe",
+            "args": [
+            ],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "symbolSearchPath": "${workspaceFolder}",
+            "environment": [
+            ],
+            "visualizerFile": "${workspaceFolder}/tsnc.natvis"
+        },         
+        {
+            "name": "Current File - JIT",
+            "type": "cppvsdbg",
+            "request": "launch",
+            "program": "<<TSC_CMD>>",
+            "args": [
+                "--shared-libs=<<TSC_LIB_PATH>>\\\\TypeScriptRuntime.dll",
+                "--gc-lib-path=<<GC_LIB_PATH>>",
+                "--llvm-lib-path=<<LLVM_LIB_PATH>>",
+                "--tsc-lib-path=<<TSC_LIB_PATH>>",
+                "--default-lib-path=<<DEFAULT_LIB_PATH>>",
+                "--no-default-lib",
+                "--opt",
+                "--opt_level=3",                
+                "--emit=jit",
+                "${file}"                
+            ],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "symbolSearchPath": "${workspaceFolder}",
+            "environment": [
+            ]
+        }
+    ]
+}
+)raw"
+
+#define LAUNCH_JSON_DATA_LINUX R"raw(
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Current File - EXE (Debug)",
+            "type": "cppdbg",
+            "preLaunchTask": "build EXE (Debug)",
+            "request": "launch",
+            "program": "${fileBasenameNoExtension}",
+            "args": [
+            ],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "environment": [
+            ],
+            "visualizerFile": "${workspaceFolder}/tsnc.natvis"
+        },         
+        {
+            "name": "Current File - EXE (Release)",
+            "type": "cppdbg",
+            "preLaunchTask": "build EXE (Release)",
+            "request": "launch",
+            "program": "${fileBasenameNoExtension}",
+            "args": [
+            ],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "environment": [
+            ],
+            "visualizerFile": "${workspaceFolder}/tsnc.natvis"
+        },         
+        {
+            "name": "Current File - JIT",
+            "type": "cppdbg",
+            "request": "launch",
+            "program": "<<TSC_CMD>>",
+            "args": [
+                "--shared-libs=<<TSC_LIB_PATH>>/libTypeScriptRuntime.so",
+                "--gc-lib-path=<<GC_LIB_PATH>>",
+                "--llvm-lib-path=<<LLVM_LIB_PATH>>",
+                "--tsc-lib-path=<<TSC_LIB_PATH>>",
+                "--default-lib-path=<<DEFAULT_LIB_PATH>>",
+                "--no-default-lib",
+                "--opt",
+                "--opt_level=3",                
+                "--emit=jit",
+                "${file}"                
+            ],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "environment": [
+            ]
+        }
+    ]
+}
+)raw"
