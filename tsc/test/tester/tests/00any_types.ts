a076ff1d59e731744a7e81bde688228f9021b6d3
ASDAlexander77 <duzhar@googlemail.com>
2024-12-29 22:58:44 +0000

    updated test

--- tsc/test/tester/tests/00any_types.ts
+++ tsc/test/tester/tests/00any_types.ts
@@ -8,8 +8,8 @@ class Cls1 extends SuperCls1
     public i: int;
 }
 
-function main() {
-
+function class_any()
+{
     const a = new Cls1();
     a.i = 10;
     a.s = 20;
@@ -19,6 +19,23 @@ function main() {
     const unboxed = box as Cls1;
 
     assert(unboxed.s == 20);
+}
+
+function array_any()
+{
+    let a = [1, 2, 3];
+
+    const box = a as any;
+
+    const unboxed = box as int[];    
+
+    assert(unboxed.length == a.length);
+}
+
+function main() {
+
+    class_any();
+    array_any();
 
     print("done.");
 }
