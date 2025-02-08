99f8f1629a3283288f0e6c88bc9bd5940865ab71
ASDAlexander77 <duzhar@googlemail.com>
2025-02-08 17:21:40 +0000

    updated build scripts

--- tag.bat
+++ tag.bat
@@ -1,2 +1,2 @@
-git tag -a v0.0-pre-alpha68 -m "pre alpha v0.0-68"
+git tag -a v0.0-pre-alpha69 -m "pre alpha v0.0-69"
 git push origin --tags
--- tag_del.bat
+++ tag_del.bat
@@ -1,2 +1,2 @@
-git push --delete origin v0.0-pre-alpha68
-git tag -d v0.0-pre-alpha68
+git push --delete origin v0.0-pre-alpha69
+git tag -d v0.0-pre-alpha69
