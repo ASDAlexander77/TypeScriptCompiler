6bef74b69b464a4551d123b25887f00ba6e1a026
ASDAlexander77 <duzhar@googlemail.com>
2025-11-24 11:52:29 +0000

    feat: Update build scripts to disable shared libraries for Visual Studio and Ninja configurations

--- scripts/build_gc_debug_vs.bat
+++ scripts/build_gc_debug_vs.bat
@@ -2,6 +2,6 @@ pushd
 mkdir __build\gc\msbuild\x64\debug
 cd __build\gc\msbuild\x64\debug
 if exist "C:/Program Files/Microsoft Visual Studio/18/Professional" set EXTRA_PARAM=-DCMAKE_GENERATOR_INSTANCE="C:/Program Files/Microsoft Visual Studio/18/Professional"
-cmake ../../../../../3rdParty/gc-8.2.8 -G "Visual Studio 18 2026" -A x64 %EXTRA_PARAM% -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_INSTALL_PREFIX=../../../../../3rdParty/gc/x64/debug -Denable_threads=ON -Denable_cplusplus=OFF
+cmake ../../../../../3rdParty/gc-8.2.8 -G "Visual Studio 18 2026" -A x64 %EXTRA_PARAM% -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -Wno-dev -DCMAKE_INSTALL_PREFIX=../../../../../3rdParty/gc/x64/debug -Denable_threads=ON -Denable_cplusplus=OFF
 cmake --build . --config Debug -j 8
 popd
--- scripts/build_gc_release_vs.bat
+++ scripts/build_gc_release_vs.bat
@@ -2,7 +2,7 @@ pushd
 mkdir __build\gc\msbuild\x64\release
 cd __build\gc\msbuild\x64\release
 if exist "C:/Program Files/Microsoft Visual Studio/2022/Professional" set EXTRA_PARAM=-DCMAKE_GENERATOR_INSTANCE="C:/Program Files/Microsoft Visual Studio/2022/Professional"
-cmake ../../../../../3rdParty/gc-8.2.8 -G "Visual Studio 17 2022" -A x64 %EXTRA_PARAM% -DCMAKE_BUILD_TYPE=Release -Wno-dev -DCMAKE_INSTALL_PREFIX=../../../../../3rdParty/gc/x64/release -Denable_threads=ON -Denable_cplusplus=OFF
+cmake ../../../../../3rdParty/gc-8.2.8 -G "Visual Studio 17 2022" -A x64 %EXTRA_PARAM% -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -Wno-dev -DCMAKE_INSTALL_PREFIX=../../../../../3rdParty/gc/x64/release -Denable_threads=ON -Denable_cplusplus=OFF
 cmake --build . --config Release -j 8
 cmake --install . --config Release
 popd
