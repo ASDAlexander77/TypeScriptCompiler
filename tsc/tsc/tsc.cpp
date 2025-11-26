8e7953ac37619d879cf6836f9b0c76eb004ca524
ASDAlexander77 <duzhar@googlemail.com>
2025-11-26 12:55:42 +0000

    feat: Update debug memory leak checking configuration in main function

--- tsc/tsc/tsc.cpp
+++ tsc/tsc/tsc.cpp
@@ -230,15 +230,11 @@ int main(int argc, char **argv)
 #if _MSC_VER && _DEBUG
     // Get current flag
     int tmpFlag = _CrtSetDbgFlag( _CRTDBG_REPORT_FLAG );
-
-    // Turn on leak-checking bit.
     tmpFlag |= _CRTDBG_LEAK_CHECK_DF;
-
-    // Turn off CRT block checking bit.
-    tmpFlag &= ~_CRTDBG_CHECK_CRT_DF;
-
     // Set flag to the new value.
     _CrtSetDbgFlag( tmpFlag );
+    //_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+    _CrtSetReportFile( _CRT_ASSERT, _CRTDBG_FILE_STDERR );
 #endif
 
     // version printer
