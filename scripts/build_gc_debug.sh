dd07eb57477455da5912d2688a2afa1712ff0b9a
ASDAlexander77 <duzhar@googlemail.com>
2026-06-26 17:10:21 +0000

    Update build scripts for GC and TSC to set executable permissions and adjust library copy paths

--- scripts/build_gc_debug.sh
+++ scripts/build_gc_debug.sh
@@ -4,4 +4,4 @@ mkdir -p 3rdParty/gc/debug
 cd __build/gc/ninja/debug
 cmake ../../../../3rdParty/gc-8.2.8 -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -Wno-dev -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=../../../../3rdParty/gc/debug -Denable_threads=ON -Denable_cplusplus=OFF
 cmake --build . --config Debug -j 16
-cp ./lib* ../../../../3rdParty/gc/debug/
+cp ./libgc-lib.a ../../../../3rdParty/gc/debug/libgc.a
