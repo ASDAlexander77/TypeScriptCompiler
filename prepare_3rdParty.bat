034600ab4e59c4820fee7de0878b276da210e960
ASDAlexander77 <duzhar@googlemail.com>
2026-06-27 21:14:35 +0100

    Update CACHE_VERSION to v9 in CI workflows and remove redundant copy commands

--- prepare_3rdParty.bat
+++ prepare_3rdParty.bat
@@ -16,7 +16,6 @@ IF EXIST ".\3rdParty\llvm\x64\%BUILD%\bin" (
   cd %p%
   echo "Downloading LLVM"
   git submodule update --init --recursive
-  rem copy /Y .\docs\fix\AddCompilerRT.cmake .\3rdParty\llvm-project\compiler-rt\cmake\Modules\
   echo "Configuring LLVM (%BUILD%)"
   cd %p%
   @call scripts\config_llvm_%BUILD%_%TOOL%.bat
@@ -41,6 +40,4 @@ IF EXIST ".\3rdParty\gc\x64\%BUILD%\lib\gc.lib" (
   xcopy  /E /H /C /I /Y .\3rdParty\libatomic_ops-%LIBATOMIC_OPS_VER%\ .\3rdParty\gc-%GC_VER%\libatomic_ops\
   cd %p%
   @call scripts\build_gc_%BUILD%_%TOOL%.bat
-  rem build_gc_*_vs.bat runs "cmake --install" which installs the libs to
-  rem 3rdParty\gc\x64\%BUILD%\lib (where tsc now links from) - no extra copy needed
 )
