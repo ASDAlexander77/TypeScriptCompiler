db0804635dbcdc50862bdfb54486a040c9631e0b
ASDAlexander77 <duzhar@googlemail.com>
2026-07-08 00:46:19 +0100

    Add clarifying comments on commutative traits in TypeScriptOps.td and fix extension registration in tsland.cpp

--- tslang/tslang/tslang.cpp
+++ tslang/tslang/tslang.cpp
@@ -321,7 +321,6 @@ int main(int argc, char **argv)
 
     // If we aren't dumping the AST, then we are compiling with/to MLIR.
     mlir::DialectRegistry registry;
-    //mlir::func::registerAllExtensions(registry);
     registerAllExtensions(registry);
 
     mlir::MLIRContext mlirContext(registry);
