634e2e299fd71c58054a0d462cd78609419ecf5a
ASDAlexander77 <duzhar@googlemail.com>
2026-07-07 20:47:16 +0100

    update build files

--- tag.bat
+++ tag.bat
@@ -1,2 +1,2 @@
-git tag -a v0.0-pre-alpha76 -m "pre alpha v0.0-76"
+git tag -a v0.0-pre-alpha77 -m "pre alpha v0.0-77"
 git push origin --tags
--- tag_del.bat
+++ tag_del.bat
@@ -1,2 +1,2 @@
-git push --delete origin v0.0-pre-alpha76
-git tag -d v0.0-pre-alpha76
+git push --delete origin v0.0-pre-alpha77
+git tag -d v0.0-pre-alpha77
