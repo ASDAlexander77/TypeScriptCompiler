125d112c40b1628bfcffe1b64c65f1f4f3d1ca8f
Alex D <duzhar@googlemail.com>
2026-07-24 16:16:19 +0100

    Increase parallelism in CMake test execution for Linux and Windows workflows; update build scripts for debug and release tests (#303)

--- tslang/build_tslang_debug-test.bat
+++ tslang/build_tslang_debug-test.bat
@@ -1,6 +1,7 @@
 pushd
 cd ../__build/tslang/msbuild/x64/debug
+cmake --build . --config Debug -j 24
 set CTEST_OUTPUT_ON_FAILURE=TRUE
 set CTEST_PARALLEL_LEVEL=16
-cmake --build . --target RUN_TESTS --config Debug --parallel
+cmake --build . --target RUN_TESTS --config Debug -j 24
 popd
